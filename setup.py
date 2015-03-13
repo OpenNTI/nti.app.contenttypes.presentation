4428e4b266a2636b39ae953677fa3ee0ba3ebdbc
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-13 13:19:44 +0000

    initial checkin
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@61378 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,27 +4,19 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
+	"z3c.autoinclude.plugin": [
+		'target = nti.app',
+	],
 	'console_scripts': [
 	],
 }
 
-TESTS_REQUIRE = [
-	'nose',
-	'nose-timer',
-	'nose-pudb',
-	'nose-progressive',
-	'nose2[coverage_plugin]',
-	'pyhamcrest',
-	'nti.nose_traceback_info',
-	'nti.testing'
-]
-
 setup(
-	name='nti.contenttypes.presentation',
+	name='nti.app.contenttypes.presentation',
 	version=VERSION,
 	author='Jason Madden',
 	author_email='jason@nextthought.com',
-	description="NTI Presentation Content Types",
+	description="NTI Application Presentation Content Types",
 	long_description=codecs.open('README.rst', encoding='utf-8').read(),
 	license='Proprietary',
 	keywords='Content Presentation',
@@ -38,30 +30,10 @@ setup(
 	],
 	packages=find_packages('src'),
 	package_dir={'': 'src'},
-	namespace_packages=['nti'],
-	tests_require=TESTS_REQUIRE,
+	namespace_packages=['nti', 'nti.app', 'nti.app.contenttypes'],
 	install_requires=[
 		'setuptools',
-		'six',
-		'persistent',
-		'zope.component',
-		'zope.dublincore',
-		'zope.interface',
-		'zope.schema',
-		'zope.security',
-		'nti.common',
-		'nti.externalization',
-		'nti.mimetype',
-		'nti.ntiids',
-		'nti.schema',
-		'nti.zodb'
-	],
-	extras_require={
-		'test': TESTS_REQUIRE,
-	},
-	dependency_links=[
-		'git+https://github.com/NextThought/nti.schema.git#egg=nti.schema',
-		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
+		'nti.contenttypes.presentation'
 	],
 	entry_points=entry_points
 )
--- src/nti/app/contenttypes/presentation/tests/__init__.py
+++ src/nti/app/contenttypes/presentation/tests/__init__.py
@@ -7,15 +7,25 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
+from zope import component
+
+from nti.app.testing.application_webtest import ApplicationTestLayer
+
+from nti.testing.layers import find_test
+from nti.testing.layers import GCLayerMixin
 from nti.testing.layers import ZopeComponentLayer
 from nti.testing.layers import ConfiguringLayerMixin
+
+from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
 
 import zope.testing.cleanup
 
 class SharedConfiguringTestLayer(ZopeComponentLayer,
-                                 ConfiguringLayerMixin):
+                                 GCLayerMixin,
+                                 ConfiguringLayerMixin,
+                                 DSInjectorMixin):
 
-    set_up_packages = ('nti.contenttypes.presentation',)
+    set_up_packages = ('nti.dataserver', 'nti.app.contenttypes.presentation')
 
     @classmethod
     def setUp(cls):
@@ -27,14 +37,9 @@ class SharedConfiguringTestLayer(ZopeComponentLayer,
         zope.testing.cleanup.cleanUp()
 
     @classmethod
-    def testSetUp(cls):
-        pass
+    def testSetUp(cls, test=None):
+        cls.setUpTestDS(test)
 
     @classmethod
     def testTearDown(cls):
         pass
-
-import unittest
-
-class ContentTypesMediaLayerTest(unittest.TestCase):
-    layer = SharedConfiguringTestLayer
