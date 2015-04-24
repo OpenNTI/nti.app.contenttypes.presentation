fc165a0ada7bfff3d11769f66a3f2880e75f38aa
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-24 02:23:47 +0000

    add helper adpaters
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@63823 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contenttypes/presentation/adapters.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from zope import component
+from zope import interface
+
+from nti.contenttypes.presentation.interfaces import INTILessonOverview
+from nti.contenttypes.presentation.interfaces import INTICourseOverviewGroup
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.traversal.traversal import find_interface
+
+@interface.implementer(ICourseInstance)
+@component.adapter(INTICourseOverviewGroup)
+def _course_overview_group_to_course(group):
+	return find_interface(group, ICourseInstance, strict=False)
+
+@interface.implementer(INTILessonOverview)
+@component.adapter(INTICourseOverviewGroup)
+def _lesson_overview_to_course(group):
+	return find_interface(group, ICourseInstance, strict=False)
