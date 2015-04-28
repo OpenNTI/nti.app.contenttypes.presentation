8517bba8eb9a21f22aecd79b52bc76095d26bc04
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-04-28 16:55:52 +0000

    add helper method to unregister utils
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@64015 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/generations/evolve4.py
+++ src/nti/app/contenttypes/presentation/generations/evolve4.py
@@ -11,22 +11,53 @@ logger = __import__('logging').getLogger(__name__)
 
 generation = 4
 
+import zope.intid
+
 from zope.component.hooks import setHooks
+from zope.component.hooks import site as current_site
+
+from zope.traversing.interfaces import IEtcNamespace
+
+from nti.contenttypes.presentation import ALL_PRESENTATION_ASSETS_INTERFACES
+
+from nti.site.interfaces import IHostPolicySiteManager
 
 from ..interfaces import IPresentationAssetsIndex
 
 from .. import CATALOG_INDEX_NAME
 
+def reset_catalog(dataserver_folder):
+	lsm = dataserver_folder.getSiteManager()
+	catalog = lsm.getUtility(IPresentationAssetsIndex, name=CATALOG_INDEX_NAME)
+	catalog.reset()
+
+def remove_utilities(dataserver_folder, interfaces=ALL_PRESENTATION_ASSETS_INTERFACES):
+	lsm = dataserver_folder.getSiteManager()
+	intids = lsm.getUtility(zope.intid.IIntIds)
+	sites = lsm.getUtility(IEtcNamespace, name='hostsites')
+	for site in sites.values():
+		with current_site(site):
+			registry = site.getSiteManager()
+			if IHostPolicySiteManager.providedBy(registry):
+				unregister = registry.subscribedUnregisterUtility
+			else:
+				unregister = registry.unregisterUtility
+			
+			for provided in interfaces or ():
+				for name, comp in list(registry.getUtilitiesFor(provided)):
+					unregister(provided=provided, name=name)
+					try:
+						intids.unregister(comp)
+					except KeyError:
+						pass
+
 def do_evolve(context):
 	setHooks()
 	conn = context.connection
 	root = conn.root()
-
 	dataserver_folder = root['nti.dataserver']
-	lsm = dataserver_folder.getSiteManager()
-
-	catalog = lsm.getUtility(IPresentationAssetsIndex, name=CATALOG_INDEX_NAME)
-	catalog.reset()
+	reset_catalog(dataserver_folder)
+	remove_utilities(dataserver_folder)
 
 def evolve(context):
 	"""
