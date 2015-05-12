e2aa616d1b69329fd13e233e67fae284fdaff421
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-12 17:01:04 +0000

    mark external with INoHrefInResponse
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@65101 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/decorators.py
+++ src/nti/app/contenttypes/presentation/decorators.py
@@ -24,6 +24,7 @@ from nti.app.products.courseware.discussions import get_forum_scopes
 
 from nti.app.products.courseware.utils import get_any_enrollment
 
+from nti.app.renderers.interfaces import INoHrefInResponse
 from nti.app.renderers.decorators import AbstractAuthenticatedRequestAwareDecorator
 
 from nti.contentlibrary.interfaces import IContentPackageLibrary
@@ -53,6 +54,7 @@ from nti.contenttypes.presentation.interfaces import INTILessonOverview
 from nti.contenttypes.presentation.interfaces import INTICourseOverviewGroup
 from nti.contenttypes.presentation.interfaces import IPresentationVisibility
 
+from nti.externalization.singleton import SingletonDecorator
 from nti.externalization.interfaces import StandardExternalFields
 from nti.externalization.interfaces import IExternalObjectDecorator
 from nti.externalization.interfaces import IExternalMappingDecorator
@@ -234,3 +236,11 @@ class _NTICourseOverviewGroupDecorator(AbstractAuthenticatedRequestAwareDecorato
 			self._decorate_external_impl(context, result)
 		except Exception:
 			logger.exception("Error while decorating course overview group")
+
+@interface.implementer(IExternalObjectDecorator)
+class _NoHrefInResponseDecorator(object):
+
+	__metaclass__ = SingletonDecorator
+
+	def decorateExternalObject(self, original, external):
+		interface.alsoProvides(external, INoHrefInResponse)
