19d76ce032e52fcf952bcd6fe9d2f48e4d2b74a2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-12 20:01:38 +0000

    move to utilities from decorator
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@65121 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/utils/__init__.py
+++ src/nti/app/contenttypes/presentation/utils/__init__.py
@@ -9,17 +9,30 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from nti.app.products.courseware.utils import get_any_enrollment
+from nti.app.products.courseware.discussions import get_topic_key
+from nti.app.products.courseware.discussions import get_forum_scopes
+
 from nti.contenttypes.courses.interfaces import ES_ALL
 from nti.contenttypes.courses.interfaces import ES_CREDIT
 from nti.contenttypes.courses.interfaces import ES_PUBLIC
 from nti.contenttypes.courses.interfaces import ES_PURCHASED
 from nti.contenttypes.courses.interfaces import ES_CREDIT_DEGREE
 from nti.contenttypes.courses.interfaces import ES_CREDIT_NONDEGREE
+from nti.contenttypes.courses.interfaces import ENROLLMENT_LINEAGE_MAP
+
+from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseSubInstance
+from nti.contenttypes.courses.discussions.utils import get_discussion_key
+from nti.contenttypes.courses.discussions.interfaces import ICourseDiscussions
+from nti.contenttypes.courses.discussions.utils import get_course_for_discussion
 
 from nti.contenttypes.presentation.interfaces import PUBLIC
 from nti.contenttypes.presentation.interfaces import CREDIT
 from nti.contenttypes.presentation.interfaces import EVERYONE
 from nti.contenttypes.presentation.interfaces import PURCHASED
+
+from nti.contenttypes.presentation.interfaces import IPresentationVisibility
 
 from .registry import remove_utilities
 from .registry import remove_all_utilities
@@ -35,3 +48,66 @@ VISIBILITY_SCOPE_MAP = {
 
 def get_visibility_for_scope(scope):
 	return VISIBILITY_SCOPE_MAP.get(scope)
+
+def get_enrollment_record(context, user):
+	course = ICourseInstance(context, None)
+	result = get_any_enrollment(course, user) if course is not None else None
+	return result
+
+def get_user_visibility(user):
+	adapted = IPresentationVisibility(user, None)
+	result = adapted.visibility() if adapted is not None else None
+	return result
+		
+def is_item_visible(item, user, context=None, record=None):
+	context = item if context is None else item
+	user_visibility = get_user_visibility(user)
+	if item.visibility != EVERYONE and user_visibility != item.visibility:
+		record = get_enrollment_record(context) if record is None else record
+		scope = record.Scope if record is not None else None
+		if get_visibility_for_scope(scope) != item.visibility:
+			return False
+	return True
+
+def resolve_discussion_course_bundle(user, item, context=None, record=None):
+	"""
+	return the approproate topic according  the discussion ref and user enrollment 
+	
+	:param item: A discussion ref object 
+	:param context: An object that can be adpated to a course
+	:param record: Enrollment record if avaiable 
+	""" 
+
+	context = item if context is None else item
+	record = get_enrollment_record(context) if record is None else record
+	if record is None:
+		return None
+	scope = record.Scope
+		
+	## get course pointed by the discussion ref
+	course = get_course_for_discussion(item, context=record.CourseInstance)
+		
+	## if course is a subinstance, make sure we are enrolled in it
+	if ICourseSubInstance.providedBy(course) and course != record.CourseInstance:
+		return None
+
+	## get course discussion
+	key = get_discussion_key(item)
+	discussion = ICourseDiscussions(course).get(key) if key else None
+	scopes = discussion.scopes if discussion is None else ()
+		
+	if	(not scope) or \
+		(not scopes) or \
+		(ES_ALL not in scopes and scope not in scopes):
+		return None
+	else:
+		topic = None
+		topic_key = get_topic_key(discussion)
+		m_scope = ENROLLMENT_LINEAGE_MAP.get(scope)[0]
+		for v in course.Discussions.values():
+			## check the forum scopes against the mapped enrollment scope
+			forum_scopes = get_forum_scopes(v)
+			if m_scope in forum_scopes and topic_key in v:
+				topic = v[topic_key] ## found the topic
+				break
+		return topic
