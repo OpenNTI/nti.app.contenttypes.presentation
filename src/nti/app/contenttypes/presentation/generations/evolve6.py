82665f6f971775738e5f2d474509635be4b19ac7
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-06-12 21:15:31 +0000

    move data between indexes
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@67118 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/generations/evolve6.py
+++ src/nti/app/contenttypes/presentation/generations/evolve6.py
@@ -13,7 +13,10 @@ generation = 6
 
 from zope.component.hooks import setHooks
 
-from .. import CATALOG_INDEX_NAME
+from nti.contentlibrary.indexed_data.interfaces import IContainedObjectCatalog
+from nti.contentlibrary.indexed_data import CATALOG_INDEX_NAME as  LIB_INDEX_NAME
+
+from .. import CATALOG_INDEX_NAME as PA_INDEX_NAME
 
 from ..interfaces import IPresentationAssetsIndex
 
@@ -24,11 +27,25 @@ def do_evolve(context):
 	dataserver_folder = root['nti.dataserver']
 
 	lsm = dataserver_folder.getSiteManager()
-	catalog = lsm.getUtility(IPresentationAssetsIndex, name=CATALOG_INDEX_NAME)
-
+	pa_catalog = lsm.getUtility(IPresentationAssetsIndex, name=PA_INDEX_NAME)
+	lib_catalog = lsm.getUtility(IContainedObjectCatalog, name=LIB_INDEX_NAME)
+	
+	# move data 
+	for name in ('_type_index', '_namespace_index'):
+		src_index = getattr(pa_catalog, name)
+		tgt_index = getattr(lib_catalog, name)
+		for doc_id, value in src_index.documents_to_values.values():
+			tgt_index.index_doc(doc_id, value)
+			
+	src_index = pa_catalog._entry_index
+	tgt_index = lib_catalog._container_index
+	for doc_id, value in src_index.documents_to_values.values():
+		tgt_index.index_doc(doc_id, value)
+	
+	# remove old indexes
 	for name in ('_type_index', '_entry_index', '_namespace_index'):
-		if hasattr(catalog, name):
-			delattr(catalog, name)
+		if hasattr(pa_catalog, name):
+			delattr(pa_catalog, name)
 
 def evolve(context):
 	"""
