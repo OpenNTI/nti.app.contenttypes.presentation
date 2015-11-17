06c3767eea6fc54695875c55e1a20e2f4c620278
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-11-17 02:58:47 +0000

    complete first test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@77234 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/tests/test_asset_views.py
+++ src/nti/app/contenttypes/presentation/tests/test_asset_views.py
@@ -8,45 +8,56 @@ __docformat__ = "restructuredtext en"
 # pylint: disable=W0212,R0904
 
 from hamcrest import is_
+from hamcrest import none
 from hamcrest import is_not
-from hamcrest import contains
-from hamcrest import not_none
-from hamcrest import has_item
+from hamcrest import has_key
 from hamcrest import has_entry
-from hamcrest import has_length
 from hamcrest import assert_that
-from hamcrest import contains_string
 
 import os
 
 import simplejson
 
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
+from nti.contenttypes.presentation.media import NTIVideo
 from nti.contenttypes.presentation.utils import prepare_json_text
+
+from nti.ntiids.ntiids import find_object_with_ntiid
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 from nti.app.testing.application_webtest import ApplicationLayerTest
 
 from nti.dataserver.tests import mock_dataserver
 
-from nti.ntiids.ntiids import find_object_with_ntiid
-
 from nti.app.products.courseware.tests import InstructedCourseApplicationTestLayer
 
 class TestAssetViews(ApplicationLayerTest):
 
-    layer = InstructedCourseApplicationTestLayer
+	layer = InstructedCourseApplicationTestLayer
 
-    default_origin = b'http://janux.ou.edu'
+	default_origin = b'http://janux.ou.edu'
 
-    course_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
-    course_url = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323'
+	course_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
+	course_url = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323'
 
-    @WithSharedApplicationMockDS(testapp=True, users=True)
-    def test_post_ntivideo(self):
-        path = os.path.join(os.path.dirname(__file__), 'ntivideo.json')
-        with open(path, "r") as fp:
-            source = simplejson.loads(prepare_json_text(fp.read()))
-        source.pop('ntiid',  None)
-        assets_url = self.course_url + '/assets'
-        res = self.testapp.post_json(assets_url, source)
-        print(res.json_body)
+	@WithSharedApplicationMockDS(testapp=True, users=True)
+	def test_post_ntivideo(self):
+		path = os.path.join(os.path.dirname(__file__), 'ntivideo.json')
+		with open(path, "r") as fp:
+			source = simplejson.loads(prepare_json_text(fp.read()))
+		source.pop('ntiid', None)
+		assets_url = self.course_url + '/assets'
+		res = self.testapp.post_json(assets_url, source)
+		assert_that(res.json_body, has_entry('ntiid', is_not(none())))
+		with mock_dataserver.mock_db_trans(self.ds, 'janux.ou.edu'):
+			ntiid = res.json_body['ntiid']
+			obj = find_object_with_ntiid(ntiid)
+			assert_that(obj, is_not(none()))
+			assert_that(obj, is_(NTIVideo))
+
+			entry = find_object_with_ntiid(self.course_ntiid)
+			course = ICourseInstance(entry)
+			container = IPresentationAssetContainer(course)
+			assert_that(container, has_key(ntiid))
