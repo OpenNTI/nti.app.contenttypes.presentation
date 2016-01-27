bb1cca6ebd852141628c2bc77c562d6d390c28d8
Josh Zuech <josh.zuech@nextthought.com>
2016-01-27 17:22:59 +0000

    Asset items use their course acls before denying all
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@81840 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/acl.py
+++ src/nti/app/contenttypes/presentation/acl.py
@@ -12,13 +12,9 @@ logger = __import__('logging').getLogger(__name__)
 from zope import component
 from zope import interface
 
-from zope.security.interfaces import IPrincipal
-
 from nti.common.property import Lazy
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
-
-from nti.contenttypes.courses.utils import get_course_editors
 
 from nti.contenttypes.presentation.interfaces import INTIAudio
 from nti.contenttypes.presentation.interfaces import INTIVideo
@@ -31,18 +27,15 @@ from nti.contenttypes.presentation.interfaces import IPresentationAsset
 from nti.contenttypes.presentation.interfaces import INTILessonOverview
 from nti.contenttypes.presentation.interfaces import INTICourseOverviewGroup
 
+from nti.dataserver.authorization import ROLE_ADMIN
+
+from nti.dataserver.authorization_acl import ace_allowing
+from nti.dataserver.authorization_acl import acl_from_aces
+
 from nti.dataserver.interfaces import ACE_DENY_ALL
 from nti.dataserver.interfaces import ALL_PERMISSIONS
 
 from nti.dataserver.interfaces import IACLProvider
-
-from nti.dataserver.authorization import ACT_READ
-from nti.dataserver.authorization import ROLE_ADMIN
-from nti.dataserver.authorization import ACT_CONTENT_EDIT
-from nti.dataserver.authorization import ROLE_CONTENT_ADMIN
-
-from nti.dataserver.authorization_acl import ace_allowing
-from nti.dataserver.authorization_acl import acl_from_aces
 
 from nti.traversal.traversal import find_interface
 
@@ -56,27 +49,11 @@ class BasePresentationAssetACLProvider(object):
 
 	@property
 	def __acl__(self):
-		editors = set()
-		instructors = set()
-		aces = [ace_allowing(ROLE_ADMIN, ALL_PERMISSIONS, type(self)),
-				ace_allowing(ROLE_CONTENT_ADMIN, ALL_PERMISSIONS, type(self))]
+		result = acl_from_aces( ace_allowing(ROLE_ADMIN, ALL_PERMISSIONS, type(self)) )
 		courses = get_presentation_asset_courses(self.context)
 		for course in courses or ():
-			# scopes get read access
-			for scope in course.SharingScopes.values():
-				aces.append(ace_allowing(IPrincipal(scope), ACT_READ, type(self)))
-
-			for i in course.instructors or ():
-				instructors.add(ace_allowing(i, ACT_READ, type(self)))
-
-			# Now our course content admins
-			for editor in get_course_editors(course):
-				editors.add(ace_allowing(editor, ACT_READ, type(self)))
-				editors.add(ace_allowing(editor, ACT_CONTENT_EDIT, type(self)))
-
-		aces.extend(editors)
-		aces.extend(instructors)
-		result = acl_from_aces(aces)
+			result.extend(IACLProvider(course).__acl__)
+		result.append(ACE_DENY_ALL)
 		return result
 
 @component.adapter(IPresentationAsset)
@@ -107,9 +84,7 @@ class NTITimelineACLProvider(BasePresentationAssetACLProvider):
 class NTISlideVideoACLProvider(BasePresentationAssetACLProvider):
 	pass
 
-@component.adapter(INTIMediaRoll)
-@interface.implementer(IACLProvider)
-class NTIMediaRollACLProvider(object):
+class AbstractCourseLineageACLProvider( object ):
 
 	def __init__(self, context):
 		self.context = context
@@ -120,52 +95,24 @@ class NTIMediaRollACLProvider(object):
 
 	@Lazy
 	def __acl__(self):
-		aces = [ace_allowing(ROLE_ADMIN, ALL_PERMISSIONS, type(self)),
-				ace_allowing(ROLE_CONTENT_ADMIN, ALL_PERMISSIONS, type(self))]
+		result = acl_from_aces( ace_allowing(ROLE_ADMIN, ALL_PERMISSIONS, type(self)) )
 		course = find_interface(self.context, ICourseInstance, strict=False)
-		if course is None:
-			aces.append(ACE_DENY_ALL)
-		result = acl_from_aces(aces)
+		if course is not None:
+			result.extend(IACLProvider(course).__acl__)
+		result.append(ACE_DENY_ALL)
 		return result
+
+@component.adapter(INTIMediaRoll)
+@interface.implementer(IACLProvider)
+class NTIMediaRollACLProvider(AbstractCourseLineageACLProvider):
+	pass
 
 @interface.implementer(IACLProvider)
 @component.adapter(INTICourseOverviewGroup)
-class NTICourseOverviewGroupACLProvider(object):
-
-	def __init__(self, context):
-		self.context = context
-
-	@property
-	def __parent__(self):
-		return self.context.__parent__
-
-	@Lazy
-	def __acl__(self):
-		aces = [ace_allowing(ROLE_ADMIN, ALL_PERMISSIONS, type(self)),
-				ace_allowing(ROLE_CONTENT_ADMIN, ALL_PERMISSIONS, type(self))]
-		course = find_interface(self.context, ICourseInstance, strict=False)
-		if course is None:
-			aces.append(ACE_DENY_ALL)
-		result = acl_from_aces(aces)
-		return result
+class NTICourseOverviewGroupACLProvider(AbstractCourseLineageACLProvider):
+	pass
 
 @interface.implementer(IACLProvider)
 @component.adapter(INTILessonOverview)
-class NTILessonOverviewACLProvider(object):
-
-	def __init__(self, context):
-		self.context = context
-
-	@property
-	def __parent__(self):
-		return self.context.__parent__
-
-	@Lazy
-	def __acl__(self):
-		aces = [ace_allowing(ROLE_ADMIN, ALL_PERMISSIONS, type(self)),
-				ace_allowing(ROLE_CONTENT_ADMIN, ALL_PERMISSIONS, type(self))]
-		course = find_interface(self.context, ICourseInstance, strict=False)
-		if course is None:
-			aces.append(ACE_DENY_ALL)
-		result = acl_from_aces(aces)
-		return result
+class NTILessonOverviewACLProvider(AbstractCourseLineageACLProvider):
+	pass
