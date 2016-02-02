9c65b9ab07e3756941966b3d47f270813d9b48b5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-02-02 16:47:20 +0000

    rename modules
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@82184 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ entry_points = {
 		'target = nti.app',
 	],
 	'console_scripts': [
-		"nti_sync_lessons_overviews = nti.app.contenttypes.presentation.scripts.sync_lessons_overviews:main",
-		"nti_reset_all_presentation_assets = nti.app.contenttypes.presentation.scripts.reset_all_presentation_assets:main",
+		"nti_sync_lessons_overviews = nti.app.contenttypes.presentation.scripts.nti_sync_lessons_overviews:main",
+		"nti_reset_all_presentation_assets = nti.app.contenttypes.presentation.scripts.nti_reset_all_presentation_assets:main",
 	],
 }
 
