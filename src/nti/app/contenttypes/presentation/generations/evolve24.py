eaf1fb756a719cdcded9de4c0313b1d1892fb060
Josh Zuech <josh.zuech@nextthought.com>
2016-03-29 15:16:39 +0000

    During migration, make sure our assets are indexed correctly
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@85455 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/generations/evolve24.py
+++ src/nti/app/contenttypes/presentation/generations/evolve24.py
@@ -5,26 +5,36 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import
+
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
 generation = 24
 
-import functools
+from itertools import chain
 
 from zope import component
 from zope import interface
 
 from zope.component.hooks import site, setHooks
 
+from nti.app.contenttypes.presentation.utils import component_site
+
 from nti.contentlibrary.interfaces import IContentUnit
 from nti.contentlibrary.interfaces import IContentPackageLibrary
 
+from nti.contentlibrary.indexed_data import get_library_catalog
+
 from nti.contenttypes.courses.interfaces import ICourseInstance
+from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
+from nti.contenttypes.courses.interfaces import ICourseOutlineContentNode
+
+from nti.contenttypes.presentation import iface_of_asset
 
 from nti.contenttypes.presentation.interfaces import INTIMedia
 from nti.contenttypes.presentation.interfaces import INTIMediaRef
+from nti.contenttypes.presentation.interfaces import INTISlideDeck
 from nti.contenttypes.presentation.interfaces import INTILessonOverview
 from nti.contenttypes.presentation.interfaces import INTICourseOverviewGroup
 from nti.contenttypes.presentation.interfaces import IPackagePresentationAsset
@@ -33,11 +43,11 @@ from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
 from nti.dataserver.interfaces import IDataserver
 from nti.dataserver.interfaces import IOIDResolver
 
+from nti.dataserver.traversal import find_nearest_site
+
 from nti.ntiids.ntiids import find_object_with_ntiid
 
-from nti.site.hostpolicy import run_job_in_all_host_sites
-
-from nti.dataserver.traversal import find_nearest_site
+from nti.site.hostpolicy import get_all_host_sites
 
 from nti.traversal.traversal import find_interface
 
@@ -67,7 +77,7 @@ def _get_content_package( course ):
 				packages = ()
 	return packages[0] if packages else None
 
-def _update_asset_lineage( item, group ):
+def _update_asset_lineage( current_site, item, lesson, package ):
 	old_unit = find_interface( item, IContentUnit, strict=False )
 	new_unit = None
 	if old_unit is not None:
@@ -75,29 +85,64 @@ def _update_asset_lineage( item, group ):
 		new_unit = find_object_with_ntiid( old_unit.ntiid )
 	else:
 		# No unit, use the unit the lesson points to.
-		lesson = find_interface( item, INTILessonOverview, strict=False )
-		if lesson is not None:
-			new_unit = find_object_with_ntiid( lesson.ContentNTIID )
+		lesson_node = find_interface( lesson, ICourseOutlineContentNode, strict=False )
+		if lesson_node is not None:
+			new_unit = find_object_with_ntiid( lesson_node.ContentNTIID )
 
 	if new_unit is None:
 		# None of the above, use our course content package.
-		course = ICourseInstance( group, None )
-		if course is not None:
-			new_unit = _get_content_package( course )
+		new_unit = package
 
 	if new_unit is None:
-		logger.warn( 'No content unit root found for (%s)', item.ntiid )
+		logger.warn( '[%s] No content unit root found for (%s)',
+					 current_site.__name__, item.ntiid )
 		return False
 
 	assert new_unit is not old_unit
 	container = IPresentationAssetContainer( new_unit )
 	container[item.ntiid] = item
 	item.__parent__ = new_unit
-	logger.info( 'Updated lineage (%s) (parent=%s)', item.ntiid, new_unit.ntiid )
+	logger.info( '[%s] Updated lineage (%s) (parent=%s)',
+				 current_site.__name__, item.ntiid, new_unit.ntiid )
 	return True
 
-def _update_assets(seen):
-	result = 0
+def _get_site_name( group ):
+	provided = iface_of_asset(group)
+	return component_site(group,
+						  provided=provided,
+						  name=group.ntiid)
+
+def _index_asset( current_site, item, course, package, lesson, group, site_name ):
+	catalog = get_library_catalog()
+	entry = ICourseCatalogEntry( course, None )
+	if package is None:
+		logger.warn( '[%s] No package found for item (%s)',
+					 current_site.__name__, item.ntiid )
+	if entry is None:
+		logger.warn( '[%s] No catalog entry found for item (%s)',
+					 current_site.__name__, item.ntiid )
+	namespace = package.ntiid if package else entry and entry.ntiid
+	containers = [lesson.ntiid, group.ntiid]
+	if package is not None:
+		containers.append( package.ntiid )
+	if entry is not None:
+		containers.append( entry.ntiid )
+	catalog.index(item,
+				  container_ntiids=containers,
+				  namespace=namespace,
+				  sites=site_name)
+
+	if INTISlideDeck.providedBy( item ):
+		containers.append( item.ntiid )
+		for slide_item in chain(item.Slides, item.Videos):
+			catalog.index(slide_item,
+					  	container_ntiids=containers,
+					  	namespace=namespace,
+					  	sites=site_name)
+	return True
+
+def _update_assets( seen, current_site ):
+	result = index_count = 0
 	library = component.queryUtility(IContentPackageLibrary)
 	if library is None:
 		return result
@@ -107,19 +152,37 @@ def _update_assets(seen):
 		if name in seen:
 			continue
 		seen.add(name)
-		for item in group or ():
+		if not group:
+			continue
+		lesson = find_interface( group, INTILessonOverview, strict=False )
+		course = ICourseInstance( group, None )
+		package = _get_content_package( course ) if course else None
+		site_name = _get_site_name( group )
+		for item in group:
 			if INTIMediaRef.providedBy( item ):
-				item = INTIMedia(item)
+				ref_item = item
+				item = INTIMedia( item, None )
+				if item is None:
+					# ~5 of these in alpha
+					logger.warn( '[%s] No media object found for ref (%s)',
+							 	 current_site.__name__, ref_item.ntiid )
+					continue
 			if not IPackagePresentationAsset.providedBy( item ):
 				continue
 			try:
 				# Easiest way is to check if our lineage reaches a site folder.
 				find_nearest_site( item )
 			except TypeError:
-				if _update_asset_lineage( item, group ):
+				if _update_asset_lineage( current_site, item, lesson, package ):
 					result += 1
+			# Now update index
+			index_count += 1
+			if _index_asset( current_site, item, course, package, lesson, group, site_name ):
+				index_count += 1
 
-	logger.info('%s asset(s) lineage fixed (%s)', result, library)
+	logger.info('[%s] Lineage fixed (%s) and indexed (%s)',
+				 current_site.__name__,
+				 result, index_count )
 	return result
 
 def do_evolve(context, generation=generation):
@@ -143,12 +206,14 @@ def do_evolve(context, generation=generation):
 
 		seen = set()
 		# Do not need to do this in global site.
-		run_job_in_all_host_sites(functools.partial(_update_assets, seen))
+		for current_site in get_all_host_sites():
+			with site(current_site):
+				_update_assets( seen, current_site )
 		logger.info('Dataserver evolution %s done.', generation)
 
 def evolve(context):
 	"""
-	Evolve to 24 by fixing lineage for authored assets, as well as putting them in
-	correct container.
+	Evolve to 24 by fixing lineage for authored assets, putting them in
+	the correct container, and making sure everything is indexed correctly.
 	"""
 	do_evolve(context, generation)
