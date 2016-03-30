86d53e86c47929cd2fcf9501d1c0779eb0d87deb
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-03-30 17:39:25 +0000

    add test for exporter
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@85562 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/exporter.py
+++ src/nti/app/contenttypes/presentation/exporter.py
@@ -9,31 +9,50 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+import os
 import re
+import six
 from StringIO import StringIO
 
 import simplejson
 
 from zope import interface
 
+from nti.app.products.courseware import ASSETS_FOLDER
+
+from nti.app.products.courseware.resources.interfaces import ICourseContentResource
+
+from nti.app.products.courseware.resources.utils import is_internal_file_link
+from nti.app.products.courseware.resources.utils import get_file_from_external_link
+
+from nti.contenttypes.courses.interfaces import NTI_COURSE_FILE_SCHEME
+
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseSectionExporter
 
 from nti.contenttypes.courses.utils import get_course_subinstances
 
+from nti.contenttypes.presentation import iface_of_asset
+
+from nti.contenttypes.presentation.interfaces import IItemAssetContainer
+
 from nti.externalization.externalization import to_external_object
 
+from nti.externalization.interfaces import StandardExternalFields
+
 from nti.ntiids.ntiids import find_object_with_ntiid
+
+ITEMS = StandardExternalFields.ITEMS
 
 def _outline_nodes(outline, seen):
 	result = []
 	def _recur(node):
 		ntiid = node.LessonOverviewNTIID
 		if ntiid and ntiid not in seen:
-			ntiid.add(ntiid)
+			seen.add(ntiid)
 			lesson = find_object_with_ntiid(ntiid)
 			if lesson is not None:
-				result.append(node, lesson)
+				result.append((node, lesson))
 		# parse children
 		for child in node.values():
 			_recur(child)
@@ -42,10 +61,36 @@ def _outline_nodes(outline, seen):
 	return result
 
 def safe_filename(s):
-	return re.sub(r'[/<>:"\\|?*]+', '', s) if s else s
+	return re.sub(r'[/<>:"\\|?*]+', '_', s) if s else s
 
 @interface.implementer(ICourseSectionExporter)
 class LessonOverviewsExporer(object):
+
+	def _process_asset_resources(self, asset, ext_obj, filer):
+		# examine interface
+		for name in iface_of_asset(asset):
+			if name.startswith('_'):
+				continue
+			value = getattr(asset, name, None)
+			if 	value is not None \
+				and isinstance(value, six.string_types) \
+				and is_internal_file_link(value):
+				resource = get_file_from_external_link(value)
+				if ICourseContentResource.providedBy(resource):
+					path = resource.path
+					path = os.path.split(path)[0]
+				else:
+					path = ASSETS_FOLDER
+				filer.save(resource.name, resource, bucket=path, overwrite=True)
+				path = "/" + path if not path.startswith('/') else path
+				internal = NTI_COURSE_FILE_SCHEME + path
+				ext_obj[name] = internal  # replace
+		# check 'children'
+		if IItemAssetContainer.providedBy(asset):
+			ext_items = ext_obj.get(ITEMS) or ()
+			asset_items = asset.Items if asset.Items is not None else ()
+			for item, item_ext in zip(asset_items, ext_items):
+				self._process_asset_resources(item, item_ext, filer)
 
 	def _do_export(self, context, filer, seen):
 		course = ICourseInstance(context)
@@ -58,7 +103,9 @@ class LessonOverviewsExporer(object):
 			source.seek(0)
 			# save to filer
 			name = safe_filename(node.src or lesson.ntiid)
-			filer.save(name, source, overwrite=True)
+			name = name + '.json' if not name.endswith('.json') else name
+			filer.save(name, source, overwrite=True, contentType=u"text/x-json")
+			self._process_asset_resources(lesson, ext_obj, filer)
 
 	def export(self, context, filer):
 		seen = set()
---
+++ src/nti/app/contenttypes/presentation/tests/test_exporter.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_not
+from hamcrest import has_length
+from hamcrest import assert_that
+does_not = is_not
+
+import shutil
+import tempfile
+
+from zope import component
+
+from nti.app.contenttypes.presentation.exporter import LessonOverviewsExporer
+
+from nti.cabinet.filer import DirectoryFiler
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.app.products.courseware.tests import PersistentInstructedCourseApplicationTestLayer
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.dataserver.tests import mock_dataserver
+
+class TestExporter(ApplicationLayerTest):
+
+	layer = PersistentInstructedCourseApplicationTestLayer
+
+	default_origin = b'http://janux.ou.edu'
+	entry_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
+
+	@classmethod
+	def course_entry(cls):
+		catalog = component.getUtility(ICourseCatalog)
+		for entry in catalog.iterCatalogEntries():
+			if entry.ntiid == cls.entry_ntiid:
+				return entry
+
+	@WithSharedApplicationMockDS(testapp=False, users=True)
+	def test_lesson_exporter(self):
+		tmp_dir = tempfile.mkdtemp(dir="/tmp")
+		try:
+			with mock_dataserver.mock_db_trans(self.ds, site_name='platform.ou.edu'):
+				course = ICourseInstance(self.course_entry())
+				filer = DirectoryFiler(tmp_dir)
+				exporter = LessonOverviewsExporer()
+				exporter.export(course, filer)
+				assert_that(filer.list(), has_length(17))
+		finally:
+			shutil.rmtree(tmp_dir, True)
