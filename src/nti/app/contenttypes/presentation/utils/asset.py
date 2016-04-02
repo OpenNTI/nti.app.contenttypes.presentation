b4f147edf6a06a25f99b1aaf963c0646366bf314
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-04-02 02:59:21 +0000

    add some logging
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@85821 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/utils/asset.py
+++ src/nti/app/contenttypes/presentation/utils/asset.py
@@ -142,7 +142,10 @@ def remove_asset(item, registry=None, catalog=None):
 	name = item.ntiid
 	provided = iface_of_asset(item)
 	registry = get_registry_4_item(item, provided, name, registry=registry)
-	unregisterUtility(registry, provided=provided, name=name)
+	if not unregisterUtility(registry, provided=provided, name=name):
+		logger.warn("Could not unregister %s,%s from %s", provided, name, registry)
+	else:
+		logger.info("%s,%s has been unregistered from %s", provided, name, registry)
 	# unindex
 	catalog = get_library_catalog() if catalog is None else catalog
 	catalog.unindex(item)
