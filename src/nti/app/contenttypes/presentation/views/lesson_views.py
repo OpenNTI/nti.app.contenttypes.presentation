8ca585c3968fbe0712b9e524d8809cc1bd81e414
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-09-23 20:26:16 +0000

    Validate PUT
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@97366 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/views/lesson_views.py
+++ src/nti/app/contenttypes/presentation/views/lesson_views.py
@@ -9,19 +9,30 @@ __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
 
+from zope import component
 from zope import interface
 from zope import lifecycleevent
+
+from pyramid import httpexceptions as hexc
 
 from pyramid.view import view_config
 from pyramid.view import view_defaults
 
+from nti.app.contenttypes.presentation import MessageFactory as _m
+
+from nti.app.externalization.error import raise_json_error
+
 from nti.appserver.dataserver_pyramid_views import GenericGetView
 
+from nti.appserver.ugd_edit_views import UGDPutView
 from nti.appserver.ugd_edit_views import UGDPostView
 from nti.appserver.ugd_edit_views import UGDDeleteView
 
+from nti.assessment.interfaces import IQAssignment
+
 from nti.contenttypes.presentation.interfaces import ILessonPublicationConstraint
 from nti.contenttypes.presentation.interfaces import ILessonPublicationConstraints
+from nti.contenttypes.presentation.interfaces import IAssignmentCompletionConstraint
 
 from nti.dataserver import authorization as nauth
 
@@ -43,63 +54,92 @@ MIMETYPE = StandardExternalFields.MIMETYPE
 ITEM_COUNT = StandardExternalFields.ITEM_COUNT
 
 def render_to_external_ref(resource):
-    link = Link(target=resource)
-    interface.alsoProvides(link, ILinkExternalHrefOnly)
-    return render_link(link)
+	link = Link(target=resource)
+	interface.alsoProvides(link, ILinkExternalHrefOnly)
+	return render_link(link)
 
 @view_config(context=ILessonPublicationConstraints)
 @view_defaults(route_name='objects.generic.traversal',
-               renderer='rest',
-               request_method='GET',
-               permission=nauth.ACT_CONTENT_EDIT)
+			   renderer='rest',
+			   request_method='GET',
+			   permission=nauth.ACT_CONTENT_EDIT)
 class LessonPublicationConstraintsGetView(GenericGetView):
 
-    def _do_call(self, constraints):
-        result = LocatedExternalDict()
-        result[MIMETYPE] = constraints.mimeType
-        result[CLASS] = getattr(constraints, '__external_class_name__',
-                                constraints.__class__.__name__)
-        items = result[ITEMS] = []
-        for constraint in constraints.Items:
-            ext_obj = to_external_object(constraint)
-            ext_obj['href'] = render_to_external_ref(constraint)
-            items.append(ext_obj)
-        result[TOTAL] = result[ITEM_COUNT] = len(items)
-        result.__parent__ = self.context
-        result.__name__ = self.request.view_name
-        result.lastModified = constraints.lastModified
-        return result
+	def _do_call(self, constraints):
+		result = LocatedExternalDict()
+		result[MIMETYPE] = constraints.mimeType
+		result[CLASS] = getattr(constraints, '__external_class_name__',
+								constraints.__class__.__name__)
+		items = result[ITEMS] = []
+		for constraint in constraints.Items:
+			ext_obj = to_external_object(constraint)
+			ext_obj['href'] = render_to_external_ref(constraint)
+			items.append(ext_obj)
+		result[TOTAL] = result[ITEM_COUNT] = len(items)
+		result.__parent__ = self.context
+		result.__name__ = self.request.view_name
+		result.lastModified = constraints.lastModified
+		return result
 
-    def __call__(self):
-        return self._do_call(self.context)
+	def __call__(self):
+		return self._do_call(self.context)
 
 @view_config(context=ILessonPublicationConstraints)
 @view_defaults(route_name='objects.generic.traversal',
-               renderer='rest',
-               request_method='POST',
-               permission=nauth.ACT_CONTENT_EDIT)
+			   renderer='rest',
+			   request_method='POST',
+			   permission=nauth.ACT_CONTENT_EDIT)
 class LessonPublicationConstraintsPostView(UGDPostView):
 
-    content_predicate = ILessonPublicationConstraint.providedBy
+	content_predicate = ILessonPublicationConstraint.providedBy
 
-    def _do_call(self):
-        creator = self.remoteUser
-        constraint = self.readCreateUpdateContentObject(creator, search_owner=False)
-        constraint.creator = creator.username
-        constraint.updateLastMod()
+	def _do_call(self):
+		creator = self.remoteUser
+		constraint = self.readCreateUpdateContentObject(creator, search_owner=False)
+		constraint.creator = creator.username
+		constraint.updateLastMod()
 
-        lifecycleevent.created(constraint)
-        self.context.append(constraint) 
-        self.request.response.status_int = 201
-        return constraint
+		lifecycleevent.created(constraint)
+		self.context.append(constraint)
+		self.request.response.status_int = 201
+		return constraint
+
+@view_config(context=IAssignmentCompletionConstraint)
+@view_defaults(route_name='objects.generic.traversal',
+			   renderer='rest',
+			   request_method='PUT',
+			   permission=nauth.ACT_CONTENT_EDIT)
+class AssignmentCompletionConstraintPutView(UGDPutView):
+
+	def _check_object_constraints(self, theObject, externalValue):
+		assignments = externalValue.get('assignments')
+		if assignments is not None:
+			if not assignments:
+				raise_json_error(self.request,
+								 hexc.HTTPUnprocessableEntity,
+								 {
+									u'message': _m("Assignment lists cannot be empty."),
+									u'code': 'ConstraintValidationError',
+								 },
+								 None)
+			for ntiid in assignments:
+				if component.queryUtility(IQAssignment, name=ntiid) is None:
+					raise_json_error(self.request,
+									 hexc.HTTPUnprocessableEntity,
+									 {
+										u'message': _m("Invalid assigment."),
+										u'code': 'ConstraintValidationError',
+										u'value': ntiid,
+									 },
+									 None)
 
 @view_config(route_name="objects.generic.traversal",
-             context=ILessonPublicationConstraint,
-             renderer='rest',
-             permission=nauth.ACT_DELETE,
-             request_method='DELETE')
+			 context=ILessonPublicationConstraint,
+			 renderer='rest',
+			 permission=nauth.ACT_DELETE,
+			 request_method='DELETE')
 class LessonPublicationConstraintDeleteView(UGDDeleteView):
 
-    def _do_delete_object(self, theObject):
-        del theObject.__parent__[theObject.__name__]
-        return theObject
+	def _do_delete_object(self, theObject):
+		del theObject.__parent__[theObject.__name__]
+		return theObject
