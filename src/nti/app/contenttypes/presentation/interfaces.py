d4552d8beb5e8441e6b5d92ad11ead2c3b0d72e2
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-09-26 14:17:15 +0000

    adjust logic to evaluate constraints
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@97470 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/interfaces.py
+++ src/nti/app/contenttypes/presentation/interfaces.py
@@ -13,27 +13,26 @@ from zope.deprecation import deprecated
 
 deprecated('IPresentationAssetsIndex', 'Use lastest library implementation')
 class IPresentationAssetsIndex(interface.Interface):
-    pass
+	pass
 
 class IItemRefValidator(interface.Interface):
-    
-    def validate():
-        """
-        Return whether or not the item reference is valid
-        """
+
+	def validate():
+		"""
+		Return whether or not the item reference is valid
+		"""
 
 class ILessonPublicationPredicate(interface.Interface):
-	
+
 	def is_satisfied(self, constraint):
 		"""
-		Return whether or not a constraint is satisfied. 
+		Return whether or not a constraint is satisfied.
 		"""
-	
+
 class IAssignmentCompletionPredicate(ILessonPublicationPredicate):
-	
+
 	def is_satisfied(self, constraint):
 		"""
 		Evaluates an assignment completion constraint. Returns true
 		if all assignments are either completed or closed; false otherwise.
 		"""
-		
--- src/nti/app/contenttypes/presentation/predicates.py
+++ src/nti/app/contenttypes/presentation/predicates.py
@@ -5,7 +5,6 @@
 """
 
 from __future__ import print_function, unicode_literals, absolute_import, division
-from __builtin__ import False
 __docformat__ = "restructuredtext en"
 
 logger = __import__('logging').getLogger(__name__)
@@ -16,20 +15,22 @@ from zope import component
 from zope import interface
 
 from zope.component.hooks import site as current_site
-from zope.interface.adapter import _lookupAll as zopeLookupAll # Private func
+
+from zope.interface.adapter import _lookupAll as zopeLookupAll  # Private func
+
+from nti.app.assessment.common import has_submitted_assigment
+from nti.app.assessment.common import get_available_for_submission_ending
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
-from nti.contenttypes.presentation.interfaces import IPresentationAsset
-from nti.contenttypes.presentation.interfaces import INTILessonOverview
-from nti.contenttypes.presentation.interfaces import ILessonPublicationConstraints
+
 from nti.contenttypes.presentation import ALL_PRESENTATION_ASSETS_INTERFACES
 
-from nti.app.assessment.common import get_available_for_submission_ending
-from nti.app.assessment.common import has_submitted_assigment
+from nti.contenttypes.presentation.interfaces import INTILessonOverview
+from nti.contenttypes.presentation.interfaces import ILessonPublicationConstraints
 
 from nti.assessment.interfaces import IQAssignment
 
-from nti.coremetadata.interfaces import IPublishablePredicate
+from nti.coremetadata.interfaces import ICalendarPublishablePredicate
 
 from nti.dataserver.interfaces import ISystemUserPrincipal
 
@@ -38,7 +39,6 @@ from nti.dataserver.users.users import User
 from nti.metadata.predicates import BasePrincipalObjects
 
 from nti.site.hostpolicy import get_all_host_sites
-
 
 def lookup_all_presentation_assets(site_registry):
 	result = {}
@@ -66,41 +66,34 @@ class _PresentationAssetObjects(BasePrincipalObjects):
 				result.extend(site_components.values())
 		return result
 
-	
 @component.adapter(INTILessonOverview)
-@interface.implementer(IPublishablePredicate)
+@interface.implementer(ICalendarPublishablePredicate)
 class LessonPublishablePredicate(object):
-	
-	__slots__=()
-	
-	def __init__(self):
-		pass
-	
+
+	__slots__ = ()
+
 	def is_published(self, lesson, principal=None, *args, **kwargs):
-		
-		constraints = ILessonPublicationConstraints(lesson, None).Items
-		
-		if principal:
-			user = User.get_user(principal.id)
-		else:
-			user = None
-		
+		constraints = ILessonPublicationConstraints(lesson).Items
+		user = User.get_user(principal.id) if principal is not None else None
+		if not user:
+			return False
 		for constraint in constraints:
 			if not self.is_satisfied(constraint, lesson, user):
-				return False;
+				return False
 		return True
-	
+
 	def is_satisfied(self, constraint, lesson, user):
-		
-		course = ICourseInstance(lesson)
 		now = datetime.utcnow()
-		
+		course = ICourseInstance(lesson, None)
+		if course is None:
+			return False
 		for assignment_ntiid in constraint.assignments:
 			assignment = component.queryUtility(IQAssignment, name=assignment_ntiid)
-			due_date = get_available_for_submission_ending(assignment, lesson)
-			if current_date > due_date:
+			if assignment is None:
+				continue
+			due_date = get_available_for_submission_ending(assignment, course)
+			if now > due_date:
 				return False
-			if has_submitted_assigment(context, user, assignment):
+			if has_submitted_assigment(course, user, assignment):
 				return False
-			
 		return True
