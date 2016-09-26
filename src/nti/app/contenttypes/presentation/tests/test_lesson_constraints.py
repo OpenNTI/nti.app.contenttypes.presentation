5f8770b24f4ae1905652af2d6455583e40d271ab
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-09-26 19:35:48 +0000

    fix test for zac
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@97502 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/tests/test_lesson_constraints.py
+++ src/nti/app/contenttypes/presentation/tests/test_lesson_constraints.py
@@ -4,78 +4,55 @@
 from __future__ import print_function, unicode_literals, absolute_import, division
 __docformat__ = "restructuredtext en"
 
-from nti.app.testing.decorators import WithSharedApplicationMockDS
-from nti.app.testing.application_webtest import ApplicationLayerTest
-
-from nti.contenttypes.courses.interfaces import ICourseInstance
-
-from nti.contenttypes.presentation.interfaces import INTILessonOverview
-from nti.contenttypes.presentation.interfaces import IPresentationAssetContainer
-# from nti.contenttypes.presentation.lesson import AssignmentCompletionConstraint
-
-from nti.dataserver.tests import mock_dataserver
-
-from nti.ntiids.ntiids import find_object_with_ntiid
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
 
 from nti.app.products.courseware.tests import InstructedCourseApplicationTestLayer
 
-import os
-import simplejson
-from nti.contenttypes.presentation.utils import prepare_json_text
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.dataserver.tests import mock_dataserver
 
 STUDENT = 'ichigo'
 
-
 class TestLessonViews(ApplicationLayerTest):
-	
-	def _load_resource(self, name):
-		path = os.path.join(os.path.dirname(__file__), name)
-		with open(path, "r") as fp:
-			source = simplejson.loads(prepare_json_text(fp.read()))
-		return source
-	
-	layer = InstructedCourseApplicationTestLayer
 
-	default_origin = b'http://janux.ou.edu'
+    layer = InstructedCourseApplicationTestLayer
 
-	course_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323_SubInstances_995'
-	course_href = '/dataserver2/Objects/%s' % course_ntiid
-	
-	def _do_enroll(self):
-		admin_environ = self._make_extra_environ(username=self.default_username)
-		enroll_url = '/dataserver2/CourseAdmin/UserCourseEnroll'
-		data = {'username':STUDENT, 'ntiid': self.course_ntiid, 'scope':'ForCredit'}
-		return self.testapp.post_json(enroll_url, data, status=201, extra_environ=admin_environ)
+    default_origin = b'http://janux.ou.edu'
 
-	@WithSharedApplicationMockDS(testapp=True, users=(STUDENT,))
-	def test_application_completion_constraints(self):
-		res = self._do_enroll()
-		
-		source = self._load_resource('ntilessonoverview.json')
-		ntiid = source.pop('NTIID', None)
+    course_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
+    course_url = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323'
+    assets_url = course_url + '/assets'
 
-		# request media by outline
-		course_ext = res.json_body['CourseInstance']
-		course_href = course_ext.get( 'href' )
-		ichigo_environ = self._make_extra_environ(username=STUDENT)
-		res = self.testapp.get( course_href, extra_environ=ichigo_environ )
-		course_ext = res.json_body
-		
-# 		lesson_overview = 'tag:nextthought.com,2011-10:NTI-NTILessonOverview-Fall2015_CS_1323_0_tryt3968_4743973034483803302_0'
+    def _do_enroll(self):
+        admin_environ = self._make_extra_environ(username=self.default_username)
+        enroll_url = '/dataserver2/CourseAdmin/UserCourseEnroll'
+        data = {'username':STUDENT, 'ntiid': self.course_ntiid, 'scope':'ForCredit'}
+        return self.testapp.post_json(enroll_url, data, status=201, extra_environ=admin_environ)
 
-		lesson = 'tag:nextthought.com,2011-10:OU-HTML-CS1323_F_2015_Intro_to_Computer_Programming.lec:01.01_LESSON'
-		children = [ "tag:nextthought.com,2011-10:OU-NTIVideo-CS1323_F_2015_Intro_to_Computer_Programming.ntivideo.video_01.01.01_Welcome",
-					 "tag:nextthought.com,2011-10:OU-NTIVideo-CS1323_F_2015_Intro_to_Computer_Programming.ntivideo.video_01.01.01_Code",
-					 "tag:nextthought.com,2011-10:OU-NTIVideo-CS1323_F_2015_Intro_to_Computer_Programming.ntivideo.video_01.01.01_Obama",
-					 "tag:nextthought.com,2011-10:OU-NTIVideo-CS1323_F_2015_Intro_to_Computer_Programming.ntivideo.video_01.01.01_Java",
-					 "tag:nextthought.com,2011-10:OU-NTIVideo-CS1323_F_2015_Intro_to_Computer_Programming.ntivideo.video_01.01.01_Academic_Integrity"]
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_application_completion_constraints(self):
+        # create and enroll student
+        with mock_dataserver.mock_db_trans(self.ds):
+            self._create_user(STUDENT)
+        self._do_enroll()
 
-		publication_constraints_link = '/dataserver2/objects/' + ntiid + '/PublicationConstraints'
-		
-		constraint = {"MimeType": "application/vnd.nextthought.lesson.assignmentcompletionconstraint", 
-					'assignments':["tag:nextthought.com,2011-10:OU-NTIVideo-CS1323_F_2015_Intro_to_Computer_Programming.ntivideo.video_01.01.01_Welcome"]}
+        # get all assets
+        admin_environ = self._make_extra_environ(username=self.default_username)
+        assets_herf = '%s?accept=application/vnd.nextthought.ntilessonoverview' % self.assets_url
+        res = self.testapp.get(assets_herf, extra_environ=admin_environ)
+        lesson = res.json_body['Items'][0]['ntiid']  # grab first lesson
 
-		res = self.testapp.post(publication_constraints_link, constraint)
-# 		data = res.json_body
+        # POST constraint
+        publication_constraints_link = '/dataserver2/Objects/' + lesson + '/PublicationConstraints'
+        assignment = "tag:nextthought.com,2011-10:OU-NAQ-CS1323_F_2015_Intro_to_Computer_Programming.naq.asg.assignment:iClicker_8_26"
+        constraint = {
+            "MimeType": "application/vnd.nextthought.lesson.assignmentcompletionconstraint",
+            'assignments':[assignment]
+        }
 
-		
+        res = self.testapp.post_json(publication_constraints_link, constraint, status=201)
+        data = res.json_body
+        print(data)
