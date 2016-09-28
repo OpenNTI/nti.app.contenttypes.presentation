0942aa022b676f8e868fa94099231e3b65aa5e31
Carlos Sanchez <carlos.sanchez@nextthought.com>
2016-09-28 03:03:25 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@97627 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/tests/test_lesson_constraints.py
+++ src/nti/app/contenttypes/presentation/tests/test_lesson_constraints.py
@@ -7,87 +7,79 @@ __docformat__ = "restructuredtext en"
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
 
-from nti.app.products.courseware.tests import InstructedCourseApplicationTestLayer
+import fudge
 
-from nti.app.testing.decorators import WithSharedApplicationMockDS
-from nti.app.testing.application_webtest import ApplicationLayerTest
+from hamcrest import is_
+from hamcrest import assert_that
 
-from nti.coremetadata.interfaces import ICalendarPublishablePredicate
-from nti.coremetadata.interfaces import get_calendar_publishable_predicate
-
-from nti.contenttypes.presentation.interfaces import INTILessonOverview
-
-from nti.dataserver.tests import mock_dataserver
 from nti.dataserver.users import User
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
-import fudge
+from nti.app.testing.application_webtest import ApplicationLayerTest
 
-from hamcrest import is_
-from hamcrest import is_not
-from hamcrest import contains
-from hamcrest import has_length
-from hamcrest import assert_that
+from nti.app.products.courseware.tests import InstructedCourseApplicationTestLayer
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+from nti.dataserver.tests import mock_dataserver
 
 STUDENT = 'ichigo'
 
 class TestLessonViews(ApplicationLayerTest):
 
-    layer = InstructedCourseApplicationTestLayer
+	layer = InstructedCourseApplicationTestLayer
 
-    default_origin = b'http://janux.ou.edu'
+	default_origin = b'http://janux.ou.edu'
 
-    course_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
-    course_url = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323'
-    assets_url = course_url + '/assets'
+	course_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
+	course_url = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323'
+	assets_url = course_url + '/assets'
 
-    def _do_enroll(self):
-        admin_environ = self._make_extra_environ(username=self.default_username)
-        enroll_url = '/dataserver2/CourseAdmin/UserCourseEnroll'
-        data = {'username':STUDENT, 'ntiid': self.course_ntiid, 'scope':'ForCredit'}
-        return self.testapp.post_json(enroll_url, data, status=201, extra_environ=admin_environ)
+	def _do_enroll(self):
+		admin_environ = self._make_extra_environ(username=self.default_username)
+		enroll_url = '/dataserver2/CourseAdmin/UserCourseEnroll'
+		data = {'username':STUDENT, 'ntiid': self.course_ntiid, 'scope':'ForCredit'}
+		return self.testapp.post_json(enroll_url, data, status=201, extra_environ=admin_environ)
 
-	
-    @WithSharedApplicationMockDS(testapp=True, users=True)
-    @fudge.patch('nti.app.contenttypes.presentation.predicates.has_submitted_assigment')
-    def test_assignment_completion_constraints(self, has_submitted_assignment):
-    	
-#     	has_submitted_assignment.is_callable().returns(True)
-    	
-        # create and enroll student
-        with mock_dataserver.mock_db_trans(self.ds):
-        	self._create_user(STUDENT)
-        self._do_enroll()
-            
-        # get all assets
-        admin_environ = self._make_extra_environ(username=self.default_username)
-        assets_herf = '%s?accept=application/vnd.nextthought.ntilessonoverview' % self.assets_url
-        res = self.testapp.get(assets_herf, extra_environ=admin_environ)
-        lesson = res.json_body['Items'][0]['ntiid']  # grab first lesson
-        lesson_link = '/dataserver2/Objects/' + lesson
-        self.testapp.post(lesson_link + '/@@publish')
-        
-        # POST constraint
-        publication_constraints_link = '/dataserver2/Objects/' + lesson + '/PublicationConstraints'
-        assignment = "tag:nextthought.com,2011-10:OU-NAQ-CS1323_F_2015_Intro_to_Computer_Programming.naq.asg.assignment:iClicker_8_26"
-        constraint = {
-            "MimeType": "application/vnd.nextthought.lesson.assignmentcompletionconstraint",
-            'assignments':[assignment]
-        }
-        
-        res = self.testapp.post_json(publication_constraints_link, constraint, status=201)
-        data = res.json_body
-        
-        with mock_dataserver.mock_db_trans(self.ds, 'platform.ou.edu'):
-            lesson_object = find_object_with_ntiid(lesson)
-            
-            student = User.get_user(STUDENT)
-            
-            has_submitted_assignment.is_callable().returns(True)
-            result = lesson_object.is_published(principal=student)
-            assert_that(result, is_(True))
-            
-            has_submitted_assignment.is_callable().returns(False)
-            result = lesson_object.is_published(principal=student)
-            assert_that(result, is_(False))
+	@WithSharedApplicationMockDS(testapp=True, users=True)
+	@fudge.patch('nti.app.contenttypes.presentation.predicates.has_submitted_assigment')
+	def test_assignment_completion_constraints(self, has_submitted_assignment):
+
+		# create and enroll student
+		with mock_dataserver.mock_db_trans(self.ds):
+			self._create_user(STUDENT)
+		self._do_enroll()
+
+		# get all assets
+		admin_environ = self._make_extra_environ(username=self.default_username)
+		assets_herf = '%s?accept=application/vnd.nextthought.ntilessonoverview' % self.assets_url
+		res = self.testapp.get(assets_herf, extra_environ=admin_environ)
+
+		# grab first lesson and force-publish
+		lesson = res.json_body['Items'][0]['ntiid']
+		lesson_link = '/dataserver2/Objects/' + lesson
+		self.testapp.post(lesson_link + '/@@publish')
+
+		# POST constraint
+		publication_constraints_link = '/dataserver2/Objects/' + lesson + '/PublicationConstraints'
+		assignment = "tag:nextthought.com,2011-10:OU-NAQ-CS1323_F_2015_Intro_to_Computer_Programming.naq.asg.assignment:iClicker_8_26"
+		constraint = {
+			"MimeType": "application/vnd.nextthought.lesson.assignmentcompletionconstraint",
+			'assignments':[assignment]
+		}
+
+		self.testapp.post_json(publication_constraints_link, constraint, status=201)
+
+		with mock_dataserver.mock_db_trans(self.ds, 'platform.ou.edu'):
+			lesson_object = find_object_with_ntiid(lesson)
+
+			student = User.get_user(STUDENT)
+
+			has_submitted_assignment.is_callable().returns(True)
+			result = lesson_object.is_published(principal=student)
+			assert_that(result, is_(True))
+
+			has_submitted_assignment.is_callable().returns(False)
+			result = lesson_object.is_published(principal=student)
+			assert_that(result, is_(False))
