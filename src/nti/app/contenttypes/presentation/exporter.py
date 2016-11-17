2d42c6ea9968f53a1b4ed9621110a19e760d2f16
Josh Zuech <josh.zuech@nextthought.com>
2016-11-17 21:11:34 +0000

    Pass salt
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@100975 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/exporter.py
+++ src/nti/app/contenttypes/presentation/exporter.py
@@ -109,7 +109,7 @@ class LessonOverviewsExporter(BaseSectionExporter):
 					ext_items = ext_obj.get(name) or ()
 					deck_items = getattr(asset, name, None) or ()
 					for item, item_ext in zip(deck_items, ext_items):
-						self._post_process_asset(item, item_ext, filer, backup)
+						self._post_process_asset(item, item_ext, filer, backup, salt)
 			else:
 				ext_items = ext_obj.get(ITEMS) or ()
 				asset_items = asset.Items if asset.Items is not None else ()
@@ -117,7 +117,7 @@ class LessonOverviewsExporter(BaseSectionExporter):
 					if not item_ext.get(NTIID) or not item_ext.get(INTERNAL_NTIID): # check valid NTIID
 						item_ext.pop(NTIID, None)
 						item_ext.pop(INTERNAL_NTIID, None)
-					self._post_process_asset(item, item_ext, filer, backup)
+					self._post_process_asset(item, item_ext, filer, backup, salt)
 		# check references to authored evaluations
 		elif	not backup \
 			and INTIAssessmentRef.providedBy(asset) \
