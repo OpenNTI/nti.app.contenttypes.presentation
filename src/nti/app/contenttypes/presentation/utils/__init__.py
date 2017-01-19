638ad4d9691e8cd8b724ecf78efc01f40b4559bd
Josh Zuech <josh.zuech@nextthought.com>
2017-01-19 02:38:32 +0000

    Move course discussion resolver to contenttypes courses
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@104760 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/utils/__init__.py
+++ src/nti/app/contenttypes/presentation/utils/__init__.py
@@ -56,7 +56,8 @@ from nti.contenttypes.courses.discussions.utils import get_course_for_discussion
 from nti.contenttypes.courses.interfaces import ICourseSubInstance
 from nti.contenttypes.courses.interfaces import IAnonymouslyAccessibleCourseInstance
 
-from nti.contenttypes.courses.utils import get_any_enrollment
+# re-export
+from nti.contenttypes.courses.discussions.utils import resolve_discussion_course_bundle
 
 from nti.contenttypes.presentation.interfaces import PUBLIC
 from nti.contenttypes.presentation.interfaces import CREDIT
@@ -73,131 +74,56 @@ from nti.ntiids.ntiids import make_specific_safe
 
 #: Visibility scope map
 VISIBILITY_SCOPE_MAP = {
-	ES_ALL: EVERYONE,
-	ES_PUBLIC: PUBLIC,
-	ES_CREDIT: CREDIT,
-	ES_PURCHASED: PURCHASED,
-	ES_CREDIT_DEGREE: CREDIT,
-	ES_CREDIT_NONDEGREE: CREDIT,
+    ES_ALL: EVERYONE,
+    ES_PUBLIC: PUBLIC,
+    ES_CREDIT: CREDIT,
+    ES_PURCHASED: PURCHASED,
+    ES_CREDIT_DEGREE: CREDIT,
+    ES_CREDIT_NONDEGREE: CREDIT,
 }
 
+
 def get_visibility_for_scope(scope):
-	return VISIBILITY_SCOPE_MAP.get(scope)
+    return VISIBILITY_SCOPE_MAP.get(scope)
+
 
 def get_user_visibility(user):
-	adapted = IPresentationVisibility(user, None)
-	result = adapted.visibility() if adapted is not None else None
-	return result
+    adapted = IPresentationVisibility(user, None)
+    result = adapted.visibility() if adapted is not None else None
+    return result
+
 
 def get_participation_principal():
-	return current_principal(False)
+    return current_principal(False)
+
 
 def _get_scope(user, context, record):
-	if user is not None:
-		record = get_enrollment_record(context, user) if record is None else record
-	else:
-		user = get_participation_principal()
+    if user is not None:
+        record = get_enrollment_record(
+            context, user) if record is None else record
+    else:
+        user = get_participation_principal()
 
-	scope = record.Scope if record is not None else None
-	if 		scope is None \
-		and IAnonymouslyAccessibleCourseInstance.providedBy(context) \
-		and IUnauthenticatedPrincipal.providedBy(user):
-		# If our context allows anonymous access, we should treat
-		# anonymous users as Open for visibility checks.
-		scope = ES_PUBLIC
-	return scope
+    scope = record.Scope if record is not None else None
+    if         scope is None \
+            and IAnonymouslyAccessibleCourseInstance.providedBy(context) \
+            and IUnauthenticatedPrincipal.providedBy(user):
+        # If our context allows anonymous access, we should treat
+        # anonymous users as Open for visibility checks.
+        scope = ES_PUBLIC
+    return scope
+
 
 def is_item_visible(item, user, context=None, record=None):
-	context = item if context is None else context
-	user_visibility = get_user_visibility(user)
-	# If it has non-everyone visibility, unequal to our user's, check scope.
-	if 		item.visibility \
-		and item.visibility != EVERYONE \
-		and user_visibility != item.visibility:
-		scope = _get_scope(user, context, record)
-		if scope != ES_ALL and get_visibility_for_scope(scope) != item.visibility:
-			# Our item is scoped and not-visible to us, but editors always have access.
-			return has_permission(ACT_CONTENT_EDIT, context)
-	return True
-
-def get_scope_term(name):
-	return ENROLLMENT_SCOPE_MAP.get(name)
-
-def get_implied_by_scopes(scopes=()):
-	result = set()
-	for scope in scopes or ():
-		result.add(scope)
-		if scope == ES_ALL:
-			result.discard(scope)
-			result.update(ENROLLMENT_SCOPE_MAP.keys())
-			break
-		else:
-			es = ENROLLMENT_SCOPE_MAP.get(scope)
-			result.update(es.implied_by if es is not None else ())
-	return result
-
-def resolve_discussion_course_bundle(user, item, context=None, record=None):
-	"""
-	return a tuple of course discussion and preferred topic according the discussion ref
-	and user enrollment or None
-
-	:param item: A discussion ref object
-	:param context: An object that can be adpated to a course
-	:param record: Enrollment record if available
-	"""
-
-	context = item if context is None else item
-	record = get_enrollment_record(context, user) if record is None else record
-	if record is None:
-		logger.warn("No enrollment record for user %s under %s", user, context)
-		return None
-
-	# enrollment scope. When scope is equals to 'All' it means user is an instructor
-	scope = record.Scope
-
-	# get course pointed by the discussion ref
-	course = get_course_for_discussion(item, context=record.CourseInstance)
-	if course is None:
-		logger.warn("No course found for discussion %s", item)
-		return None
-
-	# if course is a subinstance, make sure we are enrolled in it and
-	# we are not an instructor
-	if 		ICourseSubInstance.providedBy(course) \
-		and	scope != ES_ALL \
-		and course != record.CourseInstance:
-		return None
-
-	# get course discussion
-	key = get_discussion_key(item)
-	discussion = ICourseDiscussions(course).get(key) if key else None
-	scopes = get_implied_by_scopes(discussion.scopes) if discussion is not None else ()
-	logger.debug("Implied scopes for %s are %s", key, scopes)
-
-	if		(not scope) \
-		or	(not scopes) \
-		or	(scope != ES_ALL and ES_ALL not in scopes and scope not in scopes):
-		logger.warn("User scope %s did not match implied scopes %s", scope, scopes)
-		return None
-	else:
-		topic = None
-		topic_key = get_topic_key(discussion)
-		topic_title = make_specific_safe(discussion.title)
-		m_scope = ES_ALL if scope == ES_ALL else ENROLLMENT_LINEAGE_MAP.get(scope)[0]
-		m_scope_term = get_scope_term(m_scope) if m_scope != ES_ALL else None
-		m_scope_implies = set(getattr(m_scope_term, 'implies', None) or ())
-		for v in course.Discussions.values():
-			# check the forum scopes against the mapped enrollment scope
-			forum_scopes = get_forum_scopes(v) if m_scope != ES_ALL else ()
-			if		m_scope == ES_ALL \
-				or	m_scope in forum_scopes \
-				or	m_scope_implies.intersection(forum_scopes):
-				if topic_key in v:
-					topic = v[topic_key]
-					break
-				elif topic_title in v:
-					topic = v[topic_title]
-					break
-		if topic is not None:
-			return (discussion, topic)
-		return None
+    context = item if context is None else context
+    user_visibility = get_user_visibility(user)
+    # If it has non-everyone visibility, unequal to our user's, check scope.
+    if 		item.visibility \
+            and item.visibility != EVERYONE \
+            and user_visibility != item.visibility:
+        scope = _get_scope(user, context, record)
+        if scope != ES_ALL and get_visibility_for_scope(scope) != item.visibility:
+            # Our item is scoped and not-visible to us, but editors always have
+            # access.
+            return has_permission(ACT_CONTENT_EDIT, context)
+    return True
--- src/nti/app/contenttypes/presentation/utils/course.py
+++ src/nti/app/contenttypes/presentation/utils/course.py
@@ -10,35 +10,21 @@ __docformat__ = "restructuredtext en"
 logger = __import__('logging').getLogger(__name__)
 
 from zope import component
-from zope import interface
 
 from zope.component.hooks import getSite
 from zope.component.hooks import site as current_site
 
-from zope.container.contained import Contained
-
-from zope.security.interfaces import IPrincipal
-
-from nti.appserver.pyramid_authorization import has_permission
-
-from nti.base.mixins import CreatedAndModifiedTimeMixin
-
 from nti.contentlibrary.indexed_data import get_library_catalog
 
-from nti.contenttypes.courses.interfaces import ES_ALL
 from nti.contenttypes.courses.interfaces import NTIID_ENTRY_TYPE
 
 from nti.contenttypes.courses.interfaces import ICourseInstance
 from nti.contenttypes.courses.interfaces import ICourseCatalogEntry
 from nti.contenttypes.courses.interfaces import IPersistentCourseCatalog
-from nti.contenttypes.courses.interfaces import ICourseInstanceEnrollmentRecord
 
-from nti.contenttypes.courses.utils import get_any_enrollment
-from nti.contenttypes.courses.utils import get_course_hierarchy
 from nti.contenttypes.courses.utils import get_courses_for_packages
-from nti.contenttypes.courses.utils import is_course_instructor_or_editor
-
-from nti.dataserver.authorization import ACT_CONTENT_EDIT
+# re-export
+from nti.contenttypes.courses.utils import get_user_or_instructor_enrollment_record as get_enrollment_record
 
 from nti.ntiids.ntiids import is_ntiid_of_type
 from nti.ntiids.ntiids import find_object_with_ntiid
@@ -46,36 +32,6 @@ from nti.ntiids.ntiids import find_object_with_ntiid
 from nti.site.hostpolicy import get_host_site
 
 from nti.site.site import get_component_hierarchy_names
-
-
-@interface.implementer(ICourseInstanceEnrollmentRecord)
-class ProxyEnrollmentRecord(CreatedAndModifiedTimeMixin, Contained):
-
-    Scope = None
-    Principal = None
-    CourseInstance = None
-
-    def __init__(self, course=None, principal=None, scope=None):
-        self.Scope = scope
-        self.Principal = principal
-        self.CourseInstance = course
-
-
-def get_enrollment_record(context, user):
-    course = ICourseInstance(context, None)  # e.g. course in lineage
-    if course is None:
-        return None
-    else:
-        is_editor = has_permission(ACT_CONTENT_EDIT, course)
-        # give priority to course in lineage before checking the rest
-        for instance in get_course_hierarchy(course):
-            if is_course_instructor_or_editor(instance, user) or is_editor:
-                # create a fake enrollment record w/ all scopes to signal an
-                # instructor
-                return ProxyEnrollmentRecord(course, IPrincipal(user), ES_ALL)
-        # find any enrollment
-        result = get_any_enrollment(course, user)
-        return result
 
 
 def get_courses_for_pacakge(ntiid):
