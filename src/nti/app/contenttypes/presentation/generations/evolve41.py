e1fed3f5fefa432f1e059085365d404cc4697f77
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-03-06 23:00:14 +0000

    add evolve41
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@108127 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contenttypes/presentation/generations/evolve41.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+generation = 41
+
+from zope import component
+from zope import interface
+
+from zope.component.hooks import site as current_site
+
+from zope.intid.interfaces import IIntIds
+
+from persistent.wref import WeakRef as PersistentWeakRef
+
+from nti.app.products.courseware.resources.adapters import course_resources
+
+from nti.contentfolder.interfaces import INamedContainer
+
+from nti.contenttypes.courses.interfaces import ICourseCatalog
+from nti.contenttypes.courses.interfaces import ICourseInstance
+
+from nti.contenttypes.presentation.interfaces import IPresentationAsset
+
+from nti.dataserver.interfaces import IDataserver
+from nti.dataserver.interfaces import IOIDResolver
+
+from nti.site.hostpolicy import get_all_host_sites
+
+
+@interface.implementer(IDataserver)
+class MockDataserver(object):
+
+    root = None
+
+    def get_by_oid(self, oid, ignore_creator=False):
+        resolver = component.queryUtility(IOIDResolver)
+        if resolver is None:
+            logger.warn("Using dataserver without a proper ISiteManager.")
+        else:
+            return resolver.get_object_by_oid(oid, ignore_creator=ignore_creator)
+        return None
+
+
+def process_course_resources(container):
+    for value in list(container.values()):
+        if INamedContainer.providedBy(value):
+            process_course_resources(value)
+        elif '_associations' in value.__dict__:
+            for w in list(value._associations or ()):
+                if isinstance(w, PersistentWeakRef):
+                    obj = w()
+                    if not IPresentationAsset.providedBy(obj):
+                        continue
+                    value.remove_association(w)
+                    value.add_association(obj)
+
+
+def _process_site(current, intids, seen):
+    with current_site(current):
+        catalog = component.queryUtility(ICourseCatalog)
+        if catalog is None or catalog.isEmpty():
+            return
+        for entry in catalog.iterCatalogEntries():
+            course = ICourseInstance(entry, None)
+            doc_id = intids.queryId(course)
+            if doc_id is None or doc_id in seen:
+                continue
+            seen.add(doc_id)
+            resources = course_resources(course, create=False)
+            if resources:
+                process_course_resources(resources, intids)
+
+
+def do_evolve(context, generation=generation):
+    conn = context.connection
+    ds_folder = conn.root()['nti.dataserver']
+
+    mock_ds = MockDataserver()
+    mock_ds.root = ds_folder
+    component.provideUtility(mock_ds, IDataserver)
+
+    with current_site(ds_folder):
+        assert  component.getSiteManager() == ds_folder.getSiteManager(), \
+                "Hooks not installed?"
+
+        seen = set()
+        lsm = ds_folder.getSiteManager()
+        intids = lsm.getUtility(IIntIds)
+        for current in get_all_host_sites():
+            _process_site(current, intids, seen)
+
+    component.getGlobalSiteManager().unregisterUtility(mock_ds, IDataserver)
+    logger.info('Evolution %s done.', generation)
+
+
+def evolve(context):
+    """
+    Evolve to generation 41 by changing the weak refs on presentation types
+    """
+    do_evolve(context)
