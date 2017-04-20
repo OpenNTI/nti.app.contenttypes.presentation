010804487fcd30d541de557bf65d2339962961a3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-20 22:01:24 +0000

    pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@111074 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/views/lesson_views.py
+++ src/nti/app/contenttypes/presentation/views/lesson_views.py
@@ -40,103 +40,109 @@ from nti.externalization.interfaces import StandardExternalFields
 TOTAL = StandardExternalFields.TOTAL
 ITEM_COUNT = StandardExternalFields.ITEM_COUNT
 
+
 @view_config(context=ILessonPublicationConstraints)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   request_method='GET',
-			   permission=nauth.ACT_CONTENT_EDIT)
+               renderer='rest',
+               request_method='GET',
+               permission=nauth.ACT_CONTENT_EDIT)
 class LessonPublicationConstraintsGetView(GenericGetView):
 
-	def _do_call(self, constraints):
-		result = to_external_object(constraints)
-		result[TOTAL] = result[ITEM_COUNT] = len(constraints)
-		result.__parent__ = self.context
-		result.__name__ = self.request.view_name
-		result.lastModified = constraints.lastModified
-		return result
+    def _do_call(self, constraints):
+        result = to_external_object(constraints)
+        result[TOTAL] = result[ITEM_COUNT] = len(constraints)
+        result.__parent__ = self.context
+        result.__name__ = self.request.view_name
+        result.lastModified = constraints.lastModified
+        return result
 
-	def __call__(self):
-		return self._do_call(self.context)
+    def __call__(self):
+        return self._do_call(self.context)
+
 
 @view_config(context=ILessonPublicationConstraints)
 @view_defaults(route_name='objects.generic.traversal',
-			   name='clear',
-			   renderer='rest',
-			   request_method='POST',
-			   permission=nauth.ACT_CONTENT_EDIT)
+               name='clear',
+               renderer='rest',
+               request_method='POST',
+               permission=nauth.ACT_CONTENT_EDIT)
 class LessonPublicationConstraintsClearView(AbstractAuthenticatedView):
 
-	def __call__(self):
-		self.context.clear()
-		return self.context
+    def __call__(self):
+        self.context.clear()
+        return self.context
+
 
 @view_config(context=ILessonPublicationConstraints)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   request_method='POST',
-			   permission=nauth.ACT_CONTENT_EDIT)
+               renderer='rest',
+               request_method='POST',
+               permission=nauth.ACT_CONTENT_EDIT)
 class LessonPublicationConstraintsPostView(UGDPostView):
 
-	content_predicate = ILessonPublicationConstraint.providedBy
+    content_predicate = ILessonPublicationConstraint.providedBy
 
-	def _do_call(self):
-		creator = self.remoteUser
-		constraint = self.readCreateUpdateContentObject(creator, search_owner=False)
-		constraint.creator = creator.username
-		constraint.updateLastMod()
+    def _do_call(self):
+        creator = self.remoteUser
+        constraint = self.readCreateUpdateContentObject(creator,
+                                                        search_owner=False)
+        constraint.creator = creator.username
+        constraint.updateLastMod()
 
-		try:
-			validator = ILessonPublicationConstraintValidator(constraint)
-			validator.validate()
-		except Exception as e:
-			raise_json_error(self.request,
-							 hexc.HTTPUnprocessableEntity,
-							 {
-								u'message': str(e),
-								u'code': 'ConstraintValidationError',
-							 },
-							 None)
+        try:
+            validator = ILessonPublicationConstraintValidator(constraint)
+            validator.validate()
+        except Exception as e:
+            raise_json_error(self.request,
+                             hexc.HTTPUnprocessableEntity,
+                             {
+                                 u'message': str(e),
+                                 u'code': 'ConstraintValidationError',
+                             },
+                             None)
 
-		lifecycleevent.created(constraint)
-		self.context.append(constraint)
-		lifecycleevent.added(constraint)
-		self.request.response.status_int = 201
-		return constraint
+        lifecycleevent.created(constraint)
+        self.context.append(constraint)
+        lifecycleevent.added(constraint)
+        self.request.response.status_int = 201
+        return constraint
+
 
 @view_config(context=ILessonPublicationConstraint)
 @view_defaults(route_name='objects.generic.traversal',
-			   renderer='rest',
-			   request_method='PUT',
-			   permission=nauth.ACT_CONTENT_EDIT)
+               renderer='rest',
+               request_method='PUT',
+               permission=nauth.ACT_CONTENT_EDIT)
 class LessonCompletionConstraintPutView(UGDPutView):
 
-	def updateContentObject(self, contentObject, externalValue, set_id=False, 
-							notify=True, pre_hook=None, object_hook=None):
-		result = UGDPutView.updateContentObject(self, contentObject, externalValue, 
-											  	set_id=set_id, notify=notify, 
-											  	pre_hook=pre_hook, 
-											  	object_hook=object_hook)
-		try:
-			validator = ILessonPublicationConstraintValidator(contentObject)
-			validator.validate()
-		except Exception as e:
-			raise_json_error(self.request,
-							 hexc.HTTPUnprocessableEntity,
-							 {
-								u'message': str(e),
-								u'code': 'ConstraintValidationError',
-							 },
-							 None)
-		return result
+    def updateContentObject(self, contentObject, externalValue, set_id=False,
+                            notify=True, pre_hook=None, object_hook=None):
+        result = UGDPutView.updateContentObject(self, contentObject, externalValue,
+                                                set_id=set_id, notify=notify,
+                                                pre_hook=pre_hook,
+                                                object_hook=object_hook)
+        try:
+            validator = ILessonPublicationConstraintValidator(contentObject)
+            validator.validate()
+        except Exception as e:
+            raise_json_error(self.request,
+                             hexc.HTTPUnprocessableEntity,
+                             {
+                                 u'message': str(e),
+                                 u'code': 'ConstraintValidationError',
+                             },
+                             None)
+        return result
+
 
 @view_config(route_name="objects.generic.traversal",
-			 context=ILessonPublicationConstraint,
-			 renderer='rest',
-			 permission=nauth.ACT_DELETE,
-			 request_method='DELETE')
+             context=ILessonPublicationConstraint,
+             renderer='rest',
+             permission=nauth.ACT_DELETE,
+             request_method='DELETE')
 class LessonPublicationConstraintDeleteView(UGDDeleteView):
 
-	def _do_delete_object(self, theObject):
-		del theObject.__parent__[theObject.__name__]
-		lifecycleevent.removed(theObject)
-		return theObject
+    def _do_delete_object(self, theObject):
+        del theObject.__parent__[theObject.__name__]
+        lifecycleevent.removed(theObject)
+        return theObject
