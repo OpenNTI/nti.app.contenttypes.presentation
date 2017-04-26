2ae57872f37b92d78889c5d58e3e3205a0af7eb8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-04-26 15:34:53 +0000

    require nti.publishing
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@111550 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -4,38 +4,40 @@ from setuptools import setup, find_packages
 VERSION = '0.0.0'
 
 entry_points = {
-	"z3c.autoinclude.plugin": [
-		'target = nti.app',
-	],
-	'console_scripts': [
-		"nti_remove_invalid_assets = nti.app.contenttypes.presentation.scripts.nti_remove_invalid_assets:main",
-		"nti_sync_lessons_overviews = nti.app.contenttypes.presentation.scripts.nti_sync_lessons_overviews:main",
-		"nti_remove_course_inaccessible_assets = nti.app.contenttypes.presentation.scripts.nti_remove_course_inaccessible_assets:main",
-	],
+    "z3c.autoinclude.plugin": [
+        'target = nti.app',
+    ],
+    'console_scripts': [
+        "nti_remove_invalid_assets = nti.app.contenttypes.presentation.scripts.nti_remove_invalid_assets:main",
+        "nti_sync_lessons_overviews = nti.app.contenttypes.presentation.scripts.nti_sync_lessons_overviews:main",
+        "nti_remove_course_inaccessible_assets = nti.app.contenttypes.presentation.scripts.nti_remove_course_inaccessible_assets:main",
+    ],
 }
 
 setup(
-	name='nti.app.contenttypes.presentation',
-	version=VERSION,
-	author='Jason Madden',
-	author_email='jason@nextthought.com',
-	description="NTI Application Presentation Content Types",
-	long_description=codecs.open('README.rst', encoding='utf-8').read(),
-	license='Proprietary',
-	keywords='Content Presentation',
-	classifiers=[
-		'Intended Audience :: Developers',
-		'Natural Language :: English',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python :: 2',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: Implementation :: CPython'
-	],
-	packages=find_packages('src'),
-	package_dir={'': 'src'},
-	namespace_packages=['nti', 'nti.app', 'nti.app.contenttypes'],
-	install_requires=[
-		'setuptools'
-	],
-	entry_points=entry_points
+    name='nti.app.contenttypes.presentation',
+    version=VERSION,
+    author='Jason Madden',
+    author_email='jason@nextthought.com',
+    description="NTI Application Presentation Content Types",
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    license='Proprietary',
+    keywords='content presentation pyramid',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['nti', 'nti.app', 'nti.app.contenttypes'],
+    install_requires=[
+        'setuptools',
+        'nti.publishing',
+        'nti.contenttypes.presentation',
+    ],
+    entry_points=entry_points
 )
