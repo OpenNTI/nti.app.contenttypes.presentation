8de3596c50bfadaef15dfb7695d820b94e382d29
Josh Zuech <josh.zuech@nextthought.com>
2017-05-23 21:35:23 +0000

    Remove import
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@113462 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/synchronizer.py
+++ src/nti/app/contenttypes/presentation/synchronizer.py
@@ -90,7 +90,6 @@ from nti.externalization.externalization import to_external_object
 
 from nti.externalization.interfaces import StandardExternalFields
 
-from nti.externalization.internalization import find_factory_for
 from nti.externalization.internalization import update_from_external_object
 
 from nti.intid.common import addIntId
