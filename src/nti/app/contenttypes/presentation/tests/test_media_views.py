476e266eead0fb264b99dc83dfc0649fbbc854a3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-08 22:29:53 +0000

    add simple test
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@114627 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/app/contenttypes/presentation/tests/test_media_views.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+from hamcrest import is_not
+from hamcrest import has_entry
+from hamcrest import has_length
+from hamcrest import assert_that
+does_not = is_not
+
+from nti.app.products.courseware.tests import InstructedCourseApplicationTestLayer
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
+
+from nti.app.testing.decorators import WithSharedApplicationMockDS
+
+
+class TestMediaViews(ApplicationLayerTest):
+
+    layer = InstructedCourseApplicationTestLayer
+
+    default_origin = 'http://janux.ou.edu'
+
+    video_ntiid = 'tag:nextthought.com,2011-10:OU-NTIVideo-CLC3403_LawAndJustice.ntivideo.video_17.02'
+
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    def test_transcripts(self):
+        href = '/dataserver2/Objects/%s/@@transcripts' % self.video_ntiid
+        res = self.testapp.get(href, status=200)
+        assert_that(res.json_body, has_entry('Items', has_length(1)))
