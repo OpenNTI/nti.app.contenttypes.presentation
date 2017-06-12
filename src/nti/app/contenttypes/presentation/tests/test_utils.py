92e43f8c56e9a71ec73ddef6e89e7653248ecca4
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-12 17:38:31 +0000

    clean up
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@114838 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/tests/test_utils.py
+++ src/nti/app/contenttypes/presentation/tests/test_utils.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
@@ -16,8 +16,9 @@ from hamcrest import assert_that
 
 import unittest
 
-from nti.app.contenttypes.presentation.utils import make_asset_ntiid
 from nti.app.contenttypes.presentation.utils import VISIBILITY_SCOPE_MAP
+
+from nti.app.contenttypes.presentation.utils import make_asset_ntiid
 from nti.app.contenttypes.presentation.utils import get_visibility_for_scope
 
 from nti.contenttypes.courses.interfaces import ES_ALL
@@ -36,20 +37,24 @@ from nti.contenttypes.presentation.interfaces import INTILessonOverview
 
 from nti.coremetadata.interfaces import SYSTEM_USER_NAME
 
+
 class TestUtils(unittest.TestCase):
 
-	def test_visibility_map(self):
-		assert_that(VISIBILITY_SCOPE_MAP, has_length(6))
-		assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_ALL, is_(EVERYONE)))
-		assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_CREDIT, is_(CREDIT)))
-		assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_PUBLIC, is_(PUBLIC)))
-		assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_PURCHASED, is_(PURCHASED)))
-		assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_CREDIT_DEGREE, is_(CREDIT)))
-		assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_CREDIT_NONDEGREE, is_(CREDIT)))
-		assert_that(get_visibility_for_scope(ES_ALL), is_(EVERYONE))
+    def test_visibility_map(self):
+        assert_that(VISIBILITY_SCOPE_MAP, has_length(6))
+        assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_ALL, is_(EVERYONE)))
+        assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_CREDIT, is_(CREDIT)))
+        assert_that(VISIBILITY_SCOPE_MAP, has_entry(ES_PUBLIC, is_(PUBLIC)))
+        assert_that(VISIBILITY_SCOPE_MAP, 
+					has_entry(ES_PURCHASED, is_(PURCHASED)))
+        assert_that(VISIBILITY_SCOPE_MAP, 
+					has_entry(ES_CREDIT_DEGREE, is_(CREDIT)))
+        assert_that(VISIBILITY_SCOPE_MAP, 
+					has_entry(ES_CREDIT_NONDEGREE, is_(CREDIT)))
+        assert_that(get_visibility_for_scope(ES_ALL), is_(EVERYONE))
 
-	def test_make_asset_ntiid(self):
-		ntiid = make_asset_ntiid(INTILessonOverview, extra='ICHIGO')
-		assert_that(SYSTEM_USER_NAME, is_in(ntiid))
-		assert_that('NTILessonOverview', is_in(ntiid))
-		assert_that(ntiid, ends_with('_ICHIGO'))
+    def test_make_asset_ntiid(self):
+        ntiid = make_asset_ntiid(INTILessonOverview, extra='ICHIGO')
+        assert_that(SYSTEM_USER_NAME, is_in(ntiid))
+        assert_that('NTILessonOverview', is_in(ntiid))
+        assert_that(ntiid, ends_with('_ICHIGO'))
