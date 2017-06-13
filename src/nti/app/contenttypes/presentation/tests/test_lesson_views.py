cf348d660d978893e8155e80752b57e0e2e87277
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-13 21:57:23 +0000

    pylint &  pep8
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@114980 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/tests/test_lesson_views.py
+++ src/nti/app/contenttypes/presentation/tests/test_lesson_views.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function, unicode_literals, absolute_import, division
+from __future__ import print_function, absolute_import, division
 __docformat__ = "restructuredtext en"
 
 # disable: accessing protected members, too many methods
 # pylint: disable=W0212,R0904
-
-import fudge
 
 from hamcrest import is_
 from hamcrest import none
@@ -16,150 +14,162 @@ from hamcrest import has_entry
 from hamcrest import has_length
 from hamcrest import assert_that
 
+import fudge
+
 from nti.dataserver.users import User
 
 from nti.ntiids.ntiids import find_object_with_ntiid
 
-from nti.app.testing.application_webtest import ApplicationLayerTest
-
 from nti.app.products.courseware.tests import InstructedCourseApplicationTestLayer
+
+from nti.app.testing.application_webtest import ApplicationLayerTest
 
 from nti.app.testing.decorators import WithSharedApplicationMockDS
 
 from nti.dataserver.tests import mock_dataserver
 
-STUDENT = 'ichigo'
+STUDENT = u'ichigo'
+
 
 class TestLessonViews(ApplicationLayerTest):
 
-	layer = InstructedCourseApplicationTestLayer
+    layer = InstructedCourseApplicationTestLayer
 
-	default_origin = b'http://janux.ou.edu'
+    default_origin = 'http://janux.ou.edu'
 
-	course_ntiid = 'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
-	course_url = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323'
-	assets_url = course_url + '/assets'
+    course_ntiid = u'tag:nextthought.com,2011-10:NTI-CourseInfo-Fall2015_CS_1323'
+    course_url = '/dataserver2/%2B%2Betc%2B%2Bhostsites/platform.ou.edu/%2B%2Betc%2B%2Bsite/Courses/Fall2015/CS%201323'
+    assets_url = course_url + '/assets'
 
-	def _do_enroll(self):
-		admin_environ = self._make_extra_environ(username=self.default_username)
-		enroll_url = '/dataserver2/CourseAdmin/UserCourseEnroll'
-		data = {'username':STUDENT, 'ntiid': self.course_ntiid, 'scope':'ForCredit'}
-		return self.testapp.post_json(enroll_url, data, status=201, extra_environ=admin_environ)
+    assignment = u"tag:nextthought.com,2011-10:OU-NAQ-CS1323_F_2015_Intro_to_Computer_Programming.naq.asg.assignment:iClicker_8_26"
 
-	@WithSharedApplicationMockDS(testapp=True, users=True)
-	@fudge.patch('nti.app.contenttypes.presentation.predicates.has_submitted_assigment')
-	def test_assignment_completion_constraints(self, has_submitted_assignment):
+    survey = u"tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.set.survey:KNOWING_aristotle"
 
-		# create and enroll student
-		with mock_dataserver.mock_db_trans(self.ds):
-			self._create_user(STUDENT)
-		self._do_enroll()
+    def _do_enroll(self):
+        enroll_url = '/dataserver2/CourseAdmin/UserCourseEnroll'
+        data = {
+            'username': STUDENT,
+            'ntiid': self.course_ntiid, 
+            'scope': 'ForCredit'
+        }
+        return self.testapp.post_json(enroll_url, 
+                                      data, 
+                                      status=201)
 
-		# get all assets
-		admin_environ = self._make_extra_environ(username=self.default_username)
-		assets_herf = '%s?accept=application/vnd.nextthought.ntilessonoverview' % self.assets_url
-		res = self.testapp.get(assets_herf, extra_environ=admin_environ)
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    @fudge.patch('nti.app.contenttypes.presentation.predicates.has_submitted_assigment')
+    def test_assignment_completion_constraints(self, has_submitted_assignment):
 
-		# grab first lesson and force-publish
-		lesson = res.json_body['Items'][0]['ntiid']
-		lesson_link = '/dataserver2/Objects/' + lesson
-		self.testapp.post(lesson_link + '/@@publish')
-		res = self.testapp.get(lesson_link, status=200)
-		self.require_link_href_with_rel( res.json_body, "constraints")
-		
-		# POST constraint
-		publication_constraints_link = '%s/PublicationConstraints' % lesson_link
-		assignment = "tag:nextthought.com,2011-10:OU-NAQ-CS1323_F_2015_Intro_to_Computer_Programming.naq.asg.assignment:iClicker_8_26"
-		constraint = {
-			"MimeType": "application/vnd.nextthought.lesson.assignmentcompletionconstraint",
-			'assignments':[assignment]
-		}
+        # create and enroll student
+        with mock_dataserver.mock_db_trans(self.ds):
+            self._create_user(STUDENT)
+        self._do_enroll()
 
-		res = self.testapp.post_json(publication_constraints_link, constraint, status=201)
-		assert_that(res.json_body, has_entry('OID', is_not(none())))
-		assert_that(res.json_body, has_entry('NTIID', is_not(none())))
-		ntiid = res.json_body['NTIID']
-		
-		res = self.testapp.get(publication_constraints_link, status=200)
-		assert_that(res.json_body, has_entry('Items', has_length(1)))
-	
-		with mock_dataserver.mock_db_trans(self.ds, 'platform.ou.edu'):
-			lesson_object = find_object_with_ntiid(lesson)
+        # get all assets
+        assets_herf = '%s?accept=application/vnd.nextthought.ntilessonoverview' % self.assets_url
+        res = self.testapp.get(assets_herf, status=200)
 
-			student = User.get_user(STUDENT)
+        # grab first lesson and force-publish
+        lesson = res.json_body['Items'][0]['ntiid']
+        lesson_link = '/dataserver2/Objects/' + lesson
+        self.testapp.post(lesson_link + '/@@publish')
+        res = self.testapp.get(lesson_link, status=200)
+        self.require_link_href_with_rel(res.json_body, "constraints")
 
-			has_submitted_assignment.is_callable().returns(True)
-			result = lesson_object.is_published(principal=student)
-			assert_that(result, is_(True))
+        # POST constraint
+        publication_constraints_link = '%s/PublicationConstraints' % lesson_link
+        constraint = {
+            "MimeType": "application/vnd.nextthought.lesson.assignmentcompletionconstraint",
+            'assignments': [self.assignment]
+        }
 
-			has_submitted_assignment.is_callable().returns(False)
-			result = lesson_object.is_published(principal=student)
-			assert_that(result, is_(False))
+        res = self.testapp.post_json(publication_constraints_link, 
+                                     constraint, status=201)
+        assert_that(res.json_body, has_entry('OID', is_not(none())))
+        assert_that(res.json_body, has_entry('NTIID', is_not(none())))
+        ntiid = res.json_body['NTIID']
 
-		constraint_link = '/dataserver2/Objects/' + ntiid
-		self.testapp.delete(constraint_link, status=204)
-		
-		res = self.testapp.get(publication_constraints_link, status=200)
-		assert_that(res.json_body, has_entry('Items', has_length(0)))
+        res = self.testapp.get(publication_constraints_link, status=200)
+        assert_that(res.json_body, has_entry('Items', has_length(1)))
 
-		clear_constraints_link = '%s/@@clear' % publication_constraints_link
-		self.testapp.post_json(clear_constraints_link, status=200)
-		
-	@WithSharedApplicationMockDS(testapp=True, users=True)
-	@fudge.patch('nti.app.contenttypes.presentation.predicates.has_submitted_inquiry')
-	def test_survey_completion_constraints(self, has_submitted_inquiry):	
-		
-		# create and enroll student
-		with mock_dataserver.mock_db_trans(self.ds):
-			self._create_user(STUDENT)
-		self._do_enroll()
+        with mock_dataserver.mock_db_trans(self.ds, 'platform.ou.edu'):
+            lesson_object = find_object_with_ntiid(lesson)
 
-		# get all assets
-		admin_environ = self._make_extra_environ(username=self.default_username)
-		assets_herf = '%s?accept=application/vnd.nextthought.ntilessonoverview' % self.assets_url
-		res = self.testapp.get(assets_herf, extra_environ=admin_environ)
+            student = User.get_user(STUDENT)
 
-		# grab first lesson and force-publish
-		lesson = res.json_body['Items'][0]['ntiid']
-		lesson_link = '/dataserver2/Objects/' + lesson
-		self.testapp.post(lesson_link + '/@@publish')
-		res = self.testapp.get(lesson_link, status=200)
-		self.require_link_href_with_rel( res.json_body, "constraints")
-		
-		# POST constraint
-		publication_constraints_link = '%s/PublicationConstraints' % lesson_link
-		survey = "tag:nextthought.com,2011-10:OU-NAQ-CLC3403_LawAndJustice.naq.set.survey:KNOWING_aristotle"
-		constraint = {
-			"MimeType": "application/vnd.nextthought.lesson.surveycompletionconstraint",
-			'surveys':[survey]
-		}
-		
-		res = self.testapp.post_json(publication_constraints_link, constraint, status=201)
-		assert_that(res.json_body, has_entry('OID', is_not(none())))
-		assert_that(res.json_body, has_entry('NTIID', is_not(none())))
-		ntiid = res.json_body['NTIID']
-		
-		res = self.testapp.get(publication_constraints_link, status=200)
-		assert_that(res.json_body, has_entry('Items', has_length(1)))
-	
-		with mock_dataserver.mock_db_trans(self.ds, 'platform.ou.edu'):
-			lesson_object = find_object_with_ntiid(lesson)
+            has_submitted_assignment.is_callable().returns(True)
+            result = lesson_object.is_published(principal=student)
+            assert_that(result, is_(True))
 
-			student = User.get_user(STUDENT)
+            has_submitted_assignment.is_callable().returns(False)
+            result = lesson_object.is_published(principal=student)
+            assert_that(result, is_(False))
 
-			has_submitted_inquiry.is_callable().returns(True)
-			result = lesson_object.is_published(principal=student)
-			assert_that(result, is_(True))
+        constraint_link = '/dataserver2/Objects/' + ntiid
+        self.testapp.delete(constraint_link, status=204)
 
-			has_submitted_inquiry.is_callable().returns(False)
-			result = lesson_object.is_published(principal=student)
-			assert_that(result, is_(False))
+        res = self.testapp.get(publication_constraints_link, status=200)
+        assert_that(res.json_body, has_entry('Items', has_length(0)))
 
-		constraint_link = '/dataserver2/Objects/' + ntiid
-		self.testapp.delete(constraint_link, status=204)
-		
-		res = self.testapp.get(publication_constraints_link, status=200)
-		assert_that(res.json_body, has_entry('Items', has_length(0)))
+        clear_constraints_link = '%s/@@clear' % publication_constraints_link
+        self.testapp.post_json(clear_constraints_link, status=200)
 
-		clear_constraints_link = '%s/@@clear' % publication_constraints_link
-		self.testapp.post_json(clear_constraints_link, status=200)
+    @WithSharedApplicationMockDS(testapp=True, users=True)
+    @fudge.patch('nti.app.contenttypes.presentation.predicates.has_submitted_inquiry')
+    def test_survey_completion_constraints(self, has_submitted_inquiry):
+
+        # create and enroll student
+        with mock_dataserver.mock_db_trans(self.ds):
+            self._create_user(STUDENT)
+        self._do_enroll()
+
+        # get all assets
+        assets_herf = '%s?accept=application/vnd.nextthought.ntilessonoverview' % self.assets_url
+        res = self.testapp.get(assets_herf, status=200)
+
+        # grab first lesson and force-publish
+        lesson = res.json_body['Items'][0]['ntiid']
+ 
+        lesson_link = '/dataserver2/Objects/' + lesson
+        self.testapp.post(lesson_link + '/@@publish')
+        res = self.testapp.get(lesson_link, status=200)
+        self.require_link_href_with_rel(res.json_body, "constraints")
+
+        # POST constraint
+        publication_constraints_link = '%s/PublicationConstraints' % lesson_link
+        constraint = {
+            "MimeType": "application/vnd.nextthought.lesson.surveycompletionconstraint",
+            'surveys': [self.survey]
+        }
+
+        res = self.testapp.post_json(publication_constraints_link, 
+									 constraint, 
+									 status=201)
+        assert_that(res.json_body, has_entry('OID', is_not(none())))
+        assert_that(res.json_body, has_entry('NTIID', is_not(none())))
+        ntiid = res.json_body['NTIID']
+
+        res = self.testapp.get(publication_constraints_link, status=200)
+        assert_that(res.json_body, has_entry('Items', has_length(1)))
+
+        with mock_dataserver.mock_db_trans(self.ds, 'platform.ou.edu'):
+            lesson_object = find_object_with_ntiid(lesson)
+
+            student = User.get_user(STUDENT)
+
+            has_submitted_inquiry.is_callable().returns(True)
+            result = lesson_object.is_published(principal=student)
+            assert_that(result, is_(True))
+
+            has_submitted_inquiry.is_callable().returns(False)
+            result = lesson_object.is_published(principal=student)
+            assert_that(result, is_(False))
+
+        constraint_link = '/dataserver2/Objects/' + ntiid
+        self.testapp.delete(constraint_link, status=204)
+
+        res = self.testapp.get(publication_constraints_link, status=200)
+        assert_that(res.json_body, has_entry('Items', has_length(0)))
+
+        clear_constraints_link = '%s/@@clear' % publication_constraints_link
+        self.testapp.post_json(clear_constraints_link, status=200)
