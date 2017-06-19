a84d399b3839a5a24c41a93a7ed131d0679cb5a5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-06-19 01:06:49 +0000

    opt a bit
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@115393 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- src/nti/app/contenttypes/presentation/utils/__init__.py
+++ src/nti/app/contenttypes/presentation/utils/__init__.py
@@ -92,5 +92,5 @@ def is_item_visible(item, user, context=None, record=None):
         if scope != ES_ALL and get_visibility_for_scope(scope) != item.visibility:
             # Our item is scoped and not-visible to us, but editors always have
             # access.
-            return has_permission(ACT_CONTENT_EDIT, context)
+            result = has_permission(ACT_CONTENT_EDIT, context)
     return result
