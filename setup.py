010e2ded3762475f75eae573315e8b1b008f0af3
Carlos Sanchez <carlos.sanchez@nextthought.com>
2017-09-09 05:01:07 +0000

    scripts do not make sense any longer
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.app.contenttypes.presentation/trunk@121398 3dfb626b-079f-4ab1-a9f6-4654e5377508

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ entry_points = {
     "z3c.autoinclude.plugin": [
         'target = nti.app',
     ],
-    'console_scripts': [
-        "nti_remove_invalid_assets = nti.app.contenttypes.presentation.scripts.nti_remove_invalid_assets:main",
-        "nti_sync_lessons_overviews = nti.app.contenttypes.presentation.scripts.nti_sync_lessons_overviews:main",
-        "nti_remove_inaccessible_assets = nti.app.contenttypes.presentation.scripts.nti_remove_inaccessible_assets:main",
-    ],
 }
 
 
